217ad283516ee7d69ded6b91ebaa1ddf871aefcb
day253 <9634619+day253@users.noreply.github.com>
2021-12-02 14:52:37 +0800

    first simple dag

--- dags/get_all_nasdaq.py
+++ dags/get_all_nasdaq.py
@@ -1,13 +1,9 @@
-from __future__ import print_function
-
 import os
 import sys
-import time
-from builtins import range
 from datetime import datetime
 
 from airflow.models import DAG
-from airflow.operators.python_operator import PythonOperator
+from airflow.operators.python import PythonOperator
 
 from nasdaq import Nasdaq
 
@@ -25,24 +21,11 @@ dag = DAG(
 )
 
 
-def get_all_nasdaq(random_base):
-    """This is a function that will run within the DAG execution"""
-    time.sleep(random_base)
+def get_all_nasdaq():
     Nasdaq()._get()
 
 
 run_this = PythonOperator(
     task_id='print_the_context',
-    provide_context=True,
     python_callable=get_all_nasdaq,
     dag=dag)
-
-# Generate 10 sleeping tasks, sleeping from 0 to 9 seconds respectively
-for i in range(2):
-    task = PythonOperator(
-        task_id='sleep_for_' + str(i),
-        python_callable=get_all_nasdaq,
-        op_kwargs={'random_base': float(i) / 10},
-        dag=dag)
-
-    task.set_upstream(run_this)
