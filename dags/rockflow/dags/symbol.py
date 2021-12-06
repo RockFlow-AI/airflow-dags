cf5b33293182361a7af6b95b067715ac3a435ac5
day253 <9634619+day253@users.noreply.github.com>
2021-12-06 13:02:51 +0800

    add sink to oss

--- dags/rockflow/dags/symbol.py
+++ dags/rockflow/dags/symbol.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta
 
-from airflow.models import DAG
+from airflow.models import DAG, Variable
 
 from rockflow.common.proxy import default_proxy
 from rockflow.operators.symbol import NasdaqSymbolDownloadOperator, HkexSymbolDownloadOperator
@@ -20,11 +20,17 @@ default_args = {
 with DAG("symbol_download", default_args=default_args) as dag:
     Nasdaq = NasdaqSymbolDownloadOperator(
         task_id="download_nasdaq_symbol_to_local",
+        key='airflow-symbol-raw-nasdaq/nasdaq.json',
+        region=Variable.get("REGION"),
+        bucket_name=Variable.get("BUCKET_NAME"),
         proxy=default_proxy()
     )
 
     Hkex = HkexSymbolDownloadOperator(
         task_id="download_hkex_symbol_to_local",
+        key='airflow-symbol-raw-hkex/hkex.xlsx',
+        region=Variable.get("REGION"),
+        bucket_name=Variable.get("BUCKET_NAME"),
         proxy=default_proxy()
     )
 
--- dags/rockflow/operators/symbol.py
+++ dags/rockflow/operators/symbol.py
@@ -1,6 +1,7 @@
 from typing import Optional
 
 from airflow.models import BaseOperator
+from airflow.providers.alibaba.cloud.hooks.oss import OSSHook
 
 from rockflow.common.hkex import HKEX
 from rockflow.common.nasdaq import Nasdaq
@@ -9,26 +10,46 @@ from rockflow.common.nasdaq import Nasdaq
 class NasdaqSymbolDownloadOperator(BaseOperator):
     def __init__(
             self,
+            key: str,
+            region: str,
+            bucket_name: Optional[str] = None,
+            oss_conn_id: Optional[str] = 'oss_default',
             proxy: Optional[dict] = None,
             **kwargs,
     ) -> None:
         super().__init__(**kwargs)
         self.proxy = proxy
+        self.key = key
+        self.oss_conn_id = oss_conn_id
+        self.region = region
+        self.bucket_name = bucket_name
 
     def execute(self, context):
         nasdaq = Nasdaq(proxy=self.proxy)
-        nasdaq._get()
+        r = nasdaq._get()
+        oss_hook = OSSHook(oss_conn_id=self.oss_conn_id, region=self.region)
+        oss_hook.upload_local_file(bucket_name=self.bucket_name, key=self.key, file=r.content)
 
 
 class HkexSymbolDownloadOperator(BaseOperator):
     def __init__(
             self,
+            key: str,
+            region: str,
+            bucket_name: Optional[str] = None,
+            oss_conn_id: Optional[str] = 'oss_default',
             proxy: Optional[dict] = None,
             **kwargs,
     ) -> None:
         super().__init__(**kwargs)
         self.proxy = proxy
+        self.key = key
+        self.oss_conn_id = oss_conn_id
+        self.region = region
+        self.bucket_name = bucket_name
 
     def execute(self, context):
         hkex = HKEX(proxy=self.proxy)
-        hkex._get()
+        r = hkex._get()
+        oss_hook = OSSHook(oss_conn_id=self.oss_conn_id, region=self.region)
+        oss_hook.load_string(bucket_name=self.bucket_name, key=self.key, content=r.content)
