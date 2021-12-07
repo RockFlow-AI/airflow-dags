ec7025fd1ecfd6aaf6c5afbb1d0e94fad77c4159
day253 <9634619+day253@users.noreply.github.com>
2021-12-07 18:37:15 +0800

    fix utest

--- dags/utest/common/test_nasdaq.py
+++ dags/utest/common/test_nasdaq.py
@@ -1,12 +1,30 @@
 import unittest
+from typing import Optional
 
 from rockflow.common.nasdaq import Nasdaq
-from rockflow.common.proxy import default_proxy
 
 
-class TestRockFinance(unittest.TestCase):
+class Proxy:
+    def __init__(self, host, port) -> None:
+        self.host = host
+        self.port = port
+
+    @property
+    def proxies(self) -> Optional[dict]:
+        proxies = {
+            'http': f"http://{self.host}:{self.port}",
+            'https': f"http://{self.host}:{self.port}"
+        }
+        return proxies
+
+
+def default_proxy() -> Optional[dict]:
+    return Proxy("127.0.0.1", "7890").proxies
+
+
+class TestNasdaq(unittest.TestCase):
     def test_nasdaq(self):
-        # python -m unittest test_rock_finance.TestRockFinance.test_nasdaq
+        # python -m unittest test_nasdaq.TestNasdaq.test_nasdaq
         nasdaq = Nasdaq(proxy=default_proxy())
         print(nasdaq.get().content)
 
