87a0e6ded20b672cbc8dbd0529e885694e345893
junkai dai <daijunkai@corp.rockflow.ai>
2021-12-10 16:39:18 +0800

    fix futu

--- dags/rockflow/dags/futu.py
+++ dags/rockflow/dags/futu.py
@@ -3,51 +3,37 @@ from airflow.models import DAG
 from rockflow.dags.const import MERGE_CSV_KEY, DEFAULT_PROXY, DEFAULT_REGION, DEFAULT_BUCKET_NAME, DEFAULT_DEBUG_ARGS
 from rockflow.operators.futu import *
 
-with DAG("company_profile_cn_download", default_args=DEFAULT_DEBUG_ARGS) as cn_dag:
-    futu_html_cn_prefix = "company_profile_cn_download"
-    futu_cn = FutuCnOperator(
-        key=futu_html_cn_prefix,
-        ticker="00700-HK",
-        region=DEFAULT_REGION,
-        bucket_name=DEFAULT_BUCKET_NAME,
-        proxy=DEFAULT_PROXY
-    )
-
-with DAG("company_profile_en_download", default_args=DEFAULT_DEBUG_ARGS) as en_dag:
-    futu_html_en_prefix = "company_profile_en_download"
-    futu_en = FutuEnOperator(
-        key=futu_html_en_prefix,
-        ticker="00700-HK",
-        region=DEFAULT_REGION,
-        bucket_name=DEFAULT_BUCKET_NAME,
-        proxy=DEFAULT_PROXY
-    )
-
 with DAG("company_profile_batch_download", default_args=DEFAULT_DEBUG_ARGS) as batch_dag:
-    futu_html_batch_prefix = "company_profile_batch_download"
-    futu_batch = FutuBatchOperator(
+    FutuBatchOperator(
         from_key=MERGE_CSV_KEY,
-        key=futu_html_batch_prefix,
+        key=batch_dag.dag_id,
+        region=DEFAULT_REGION,
+        bucket_name=DEFAULT_BUCKET_NAME,
+        proxy=DEFAULT_PROXY
+    )
+
+with DAG("company_profile_batch_download_debug", default_args=DEFAULT_DEBUG_ARGS) as batch_dag_debug:
+    FutuBatchOperator(
+        from_key=MERGE_CSV_KEY,
+        key=batch_dag_debug.dag_id,
         region=DEFAULT_REGION,
         bucket_name=DEFAULT_BUCKET_NAME,
         proxy=DEFAULT_PROXY
     )
 
 with DAG("company_profile_batch_extract", default_args=DEFAULT_DEBUG_ARGS) as extract_dag:
-    futu_html_extract_prefix = "company_profile_extract_download"
-    futu_extract = FutuExtractHtml(
+    FutuExtractHtml(
         from_key="company_profile_batch_download/",
-        key=futu_html_extract_prefix,
+        key=extract_dag.dag_id,
         region=DEFAULT_REGION,
         bucket_name=DEFAULT_BUCKET_NAME,
         proxy=DEFAULT_PROXY
     )
 
 with DAG("company_profile_batch_extract_debug", default_args=DEFAULT_DEBUG_ARGS) as extract_dag_debug:
-    futu_html_extract_prefix_debug = "company_profile_batch_extract_debug"
-    futu_extract_debug = FutuExtractHtmlDebug(
+    FutuExtractHtmlDebug(
         from_key="company_profile_batch_download/",
-        key=futu_html_extract_prefix_debug,
+        key=extract_dag_debug.dag_id,
         region=DEFAULT_REGION,
         bucket_name=DEFAULT_BUCKET_NAME,
         proxy=DEFAULT_PROXY
--- dags/rockflow/operators/futu.py
+++ dags/rockflow/operators/futu.py
@@ -1,5 +1,4 @@
 import json
-import os
 from itertools import islice
 from multiprocessing.pool import ThreadPool as Pool
 from pathlib import Path
@@ -7,7 +6,6 @@ from typing import Any
 
 import oss2
 import pandas as pd
-from stringcase import snakecase
 
 from rockflow.common.datatime_helper import GmtDatetimeCheck
 from rockflow.common.futu_company_profile import FutuCompanyProfileCn, FutuCompanyProfileEn, FutuCompanyProfile
@@ -61,6 +59,15 @@ class FutuBatchOperator(OSSOperator):
             axis=1,
             args=(self.key, self.proxy, self.bucket)
         )
+
+
+class FutuBatchOperatorDebug(FutuBatchOperator):
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+
+    @property
+    def symbols(self) -> pd.DataFrame:
+        return pd.read_csv(self.get_object(self.from_key))[:100]
 
 
 class FutuExtractHtml(OSSSaveOperator):
@@ -124,51 +131,3 @@ class FutuExtractHtmlDebug(FutuExtractHtml):
                     )
                 )
         return json.dumps(result, ensure_ascii=False)
-
-
-class FutuOperator(OSSSaveOperator):
-    def __init__(self,
-                 ticker: str,
-                 **kwargs) -> None:
-        if 'task_id' not in kwargs:
-            kwargs['task_id'] = f"{snakecase(self.__class__.__name__)}_{ticker}"
-        super().__init__(**kwargs)
-        self.ticker = ticker
-
-    @property
-    def key(self):
-        return os.path.join(self._key, f"{self.ticker}.html")
-
-    @property
-    def page(self):
-        raise NotImplementedError()
-
-    @property
-    def instance(self):
-        return self.page(
-            symbol=self.ticker,
-            futu_ticker=self.ticker,
-            proxy=self.proxy,
-        )
-
-    @property
-    def content(self):
-        return self.instance.get().content
-
-
-class FutuCnOperator(FutuOperator):
-    def __init__(self, **kwargs) -> None:
-        super().__init__(**kwargs)
-
-    @property
-    def page(self):
-        return FutuCompanyProfileCn
-
-
-class FutuEnOperator(FutuOperator):
-    def __init__(self, **kwargs) -> None:
-        super().__init__(**kwargs)
-
-    @property
-    def page(self):
-        return FutuCompanyProfileEn
