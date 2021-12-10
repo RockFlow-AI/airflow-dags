b89ce75a45ae0f0018f6652a97508eb919e38b7e
junkai dai <daijunkai@corp.rockflow.ai>
2021-12-10 20:36:02 +0800

    fix dag

--- dags/rockflow/dags/futu.py
+++ dags/rockflow/dags/futu.py
@@ -4,55 +4,53 @@ from rockflow.dags.const import *
 from rockflow.dags.symbol import MERGE_CSV_KEY
 from rockflow.operators.futu import *
 
-with DAG("company_profile_batch_download_cn", default_args=DEFAULT_DEBUG_ARGS) as dag:
+with DAG("company_profile_batch_download", default_args=DEFAULT_DEBUG_ARGS) as company_profile_batch_download:
     FutuBatchOperatorCn(
         from_key=MERGE_CSV_KEY,
-        key=dag.dag_id,
+        key=company_profile_batch_download.dag_id,
         region=DEFAULT_REGION,
         bucket_name=DEFAULT_BUCKET_NAME,
         proxy=DEFAULT_PROXY
     )
 
-with DAG("company_profile_batch_download_en", default_args=DEFAULT_DEBUG_ARGS) as dag:
     FutuBatchOperatorEn(
         from_key=MERGE_CSV_KEY,
-        key=dag.dag_id,
+        key=company_profile_batch_download.dag_id,
         region=DEFAULT_REGION,
         bucket_name=DEFAULT_BUCKET_NAME,
         proxy=DEFAULT_PROXY
     )
 
-with DAG("company_profile_batch_download_cn_debug", default_args=DEFAULT_DEBUG_ARGS) as dag:
+with DAG("company_profile_batch_download_debug", default_args=DEFAULT_DEBUG_ARGS) as company_profile_batch_download_debug:
     FutuBatchOperatorCnDebug(
         from_key=MERGE_CSV_KEY,
-        key=dag.dag_id,
+        key=company_profile_batch_download_debug.dag_id,
         region=DEFAULT_REGION,
         bucket_name=DEFAULT_BUCKET_NAME,
         proxy=DEFAULT_PROXY
     )
 
-with DAG("company_profile_batch_download_en_debug", default_args=DEFAULT_DEBUG_ARGS) as dag:
     FutuBatchOperatorEnDebug(
         from_key=MERGE_CSV_KEY,
-        key=dag.dag_id,
+        key=company_profile_batch_download_debug.dag_id,
         region=DEFAULT_REGION,
         bucket_name=DEFAULT_BUCKET_NAME,
         proxy=DEFAULT_PROXY
     )
 
-with DAG("company_profile_batch_extract", default_args=DEFAULT_DEBUG_ARGS) as dag:
+with DAG("company_profile_batch_extract", default_args=DEFAULT_DEBUG_ARGS) as company_profile_batch_extract:
     FutuExtractHtml(
         from_key="company_profile_batch_download/",
-        key=dag.dag_id,
+        key=company_profile_batch_extract.dag_id,
         region=DEFAULT_REGION,
         bucket_name=DEFAULT_BUCKET_NAME,
         proxy=DEFAULT_PROXY
     )
 
-with DAG("company_profile_batch_extract_debug", default_args=DEFAULT_DEBUG_ARGS) as dag:
+with DAG("company_profile_batch_extract_debug", default_args=DEFAULT_DEBUG_ARGS) as company_profile_batch_extract_debug:
     FutuExtractHtmlDebug(
         from_key="company_profile_batch_download_debug/",
-        key=dag.dag_id,
+        key=company_profile_batch_extract_debug.dag_id,
         region=DEFAULT_REGION,
         bucket_name=DEFAULT_BUCKET_NAME,
         proxy=DEFAULT_PROXY
