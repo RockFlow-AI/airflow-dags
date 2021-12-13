f4f1af08c3f7d5582b69effefe4f20cd460116d2
junkai dai <daijunkai@corp.rockflow.ai>
2021-12-13 18:26:02 +0800

    rm mysql OssToMysqlOperator

--- dags/rockflow/dags/market.py
+++ dags/rockflow/dags/market.py
@@ -1,9 +1,9 @@
 from airflow.models import DAG
-from airflow.models.baseoperator import chain
 
 from rockflow.dags.const import *
 from rockflow.operators.market import *
-from rockflow.operators.mysql import OssToMysqlOperator
+
+# from rockflow.operators.mysql import OssToMysqlOperator
 
 with DAG("market_download", default_args=DEFAULT_DEBUG_ARGS) as mic:
     mic = MicDownloadOperator(
@@ -13,15 +13,15 @@ with DAG("market_download", default_args=DEFAULT_DEBUG_ARGS) as mic:
         proxy=DEFAULT_PROXY
     )
 
-    mic_to_mysql = OssToMysqlOperator(
-        region=DEFAULT_REGION,
-        bucket_name=DEFAULT_BUCKET_NAME,
-        oss_source_key=mic.oss_key,
-        mysql_table='flow_ticker_mic',
-        mysql_conn_id=MYSQL_CONNECTION_FLOW_TICKER
-    )
-
-chain(
-    mic,
-    mic_to_mysql,
-)
+#     mic_to_mysql = OssToMysqlOperator(
+#         region=DEFAULT_REGION,
+#         bucket_name=DEFAULT_BUCKET_NAME,
+#         oss_source_key=mic.oss_key,
+#         mysql_table='flow_ticker_mic',
+#         mysql_conn_id=MYSQL_CONNECTION_FLOW_TICKER
+#     )
+#
+# chain(
+#     mic,
+#     mic_to_mysql,
+# )
