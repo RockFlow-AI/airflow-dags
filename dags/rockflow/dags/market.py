b656cc57035a384c4af868fcda71c29d0d18762e
junkai dai <daijunkai@corp.rockflow.ai>
2021-12-13 17:53:47 +0800

    mic dag

--- dags/rockflow/dags/market.py
+++ dags/rockflow/dags/market.py
@@ -13,15 +13,15 @@ with DAG("market_download", default_args=DEFAULT_DEBUG_ARGS) as mic:
         proxy=DEFAULT_PROXY
     )
 
-    mic_to_mysql = OssToMysqlOperator(
-        region=DEFAULT_REGION,
-        bucket_name=DEFAULT_BUCKET_NAME,
-        oss_source_key='',
-        mysql_table='flow_ticker_mic',
-        mysql_conn_id=MYSQL_CONNECTION_FLOW_TICKER,
-    )
-
-chain(
-    mic,
-    mic_to_mysql,
-)
+#     mic_to_mysql = OssToMysqlOperator(
+#         region=DEFAULT_REGION,
+#         bucket_name=DEFAULT_BUCKET_NAME,
+#         oss_source_key='',
+#         mysql_table='flow_ticker_mic',
+#         mysql_conn_id=MYSQL_CONNECTION_FLOW_TICKER,
+#     )
+#
+# chain(
+#     mic,
+#     mic_to_mysql,
+# )
