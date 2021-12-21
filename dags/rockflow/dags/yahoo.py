bcfed0781741209131df917bb0119a06268ad710
Yu Xie <1261863719@qq.com>
2021-12-21 18:18:49 +0800

    add yahoo_extract

--- dags/rockflow/dags/yahoo.py
+++ dags/rockflow/dags/yahoo.py
@@ -1,11 +1,12 @@
 from airflow.models import DAG
+from airflow.models.baseoperator import chain
 
 from rockflow.dags.const import *
 from rockflow.dags.symbol import MERGE_CSV_KEY
 from rockflow.operators.yahoo import *
 
 with DAG("yahoo_download", default_args=DEFAULT_DEBUG_ARGS) as yahoo_download:
-    YahooBatchOperator(
+    yahoo = YahooBatchOperator(
         from_key=MERGE_CSV_KEY,
         key=yahoo_download.dag_id,
         region=DEFAULT_REGION,
@@ -13,11 +14,33 @@ with DAG("yahoo_download", default_args=DEFAULT_DEBUG_ARGS) as yahoo_download:
         proxy=DEFAULT_PROXY
     )
 
+    yahoo_extract = YahooExtractOperator(
+        from_key="{{ task_instance.xcom_pull('" + yahoo.task_id + "') }}",
+        key="yahoo_extract",
+        region=DEFAULT_REGION,
+        bucket_name=DEFAULT_BUCKET_NAME,
+        proxy=DEFAULT_PROXY
+    )
+
 with DAG("yahoo_download_debug", default_args=DEFAULT_DEBUG_ARGS) as yahoo_download_debug:
-    YahooBatchOperatorDebug(
+    yahoo_debug = YahooBatchOperatorDebug(
         from_key=MERGE_CSV_KEY,
         key=yahoo_download_debug.dag_id,
         region=DEFAULT_REGION,
         bucket_name=DEFAULT_BUCKET_NAME,
         proxy=DEFAULT_PROXY
     )
+
+    yahoo_extract_debug = YahooExtractOperator(
+        from_key="{{ task_instance.xcom_pull('" +
+        yahoo_debug.task_id + "') }}",
+        key="yahoo_extract_debug",
+        region=DEFAULT_REGION,
+        bucket_name=DEFAULT_BUCKET_NAME,
+        proxy=DEFAULT_PROXY
+    )
+
+chain(
+    [yahoo, yahoo_debug],
+    [yahoo_extract, yahoo_extract_debug]
+)
