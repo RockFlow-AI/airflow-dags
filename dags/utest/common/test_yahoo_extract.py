dce75fc487c909b0118450afe117f6ded5e24e14
Yu Xie <1261863719@qq.com>
2021-12-22 16:47:07 +0800

    fix

--- dags/utest/common/test_yahoo_extract.py
+++ dags/utest/common/test_yahoo_extract.py
@@ -7,12 +7,32 @@ from rockflow.operators.yahoo import YahooExtractOperator
 class TestYahoo(unittest.TestCase):
     def test_yahoo(self):
         yahoo = YahooExtractOperator(
-            from_key="yahoo_download_debug_yahoo",
+            from_key="yahoo_download_debug_yahoo/",
             key="yahoo_extract",
             region=DEFAULT_REGION,
             bucket_name=DEFAULT_BUCKET_NAME
         )
         self.assertIsNotNone(yahoo.execute(""))
+
+    def test_merge(self):
+        test_data1 = [
+            [1, {"a": "a1", "b": "b1"}],
+            [2, {"a": "a2", "b": "b2"}],
+            [3, {"a": "a3", "b": "b3"}],
+            [4, {"a": "a4", "b": "b4"}]
+        ]
+        test_data2 = [
+            [1, {"a": "a1", "b": "b1"}],
+            [1, {"a": "a2", "b": "b2"}],
+            [3, {"a": "a3", "b": "b3"}],
+            [4, {"a": "a4", "b": "b4"}]
+        ]
+        self.assertTrue(
+            len(YahooExtractOperator.merge_data(test_data1)) == 2
+        )
+        self.assertFalse(
+            YahooExtractOperator.merge_data(test_data2)["a"][1] == "a2"
+        )
 
 
 if __name__ == '__main__':
