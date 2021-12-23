7f5635c30874771618a9db8eff8386ccdd9ad6fd
junkai dai <daijunkai@corp.rockflow.ai>
2021-12-23 18:34:14 +0800

    fix sync history

--- dags/rockflow/common/pandas_helper.py
+++ dags/rockflow/common/pandas_helper.py
@@ -15,7 +15,7 @@ def merge_data_frame(data_frame_list: Optional[List[pd.DataFrame]]) -> Optional[
 
 
 def map_frame(df: Optional[pd.DataFrame], mapper: Dict[str, str]) -> Optional[pd.DataFrame]:
-    result = pd.DataFrame(columns=[k for k, _ in mapper.items()])
+    result = pd.DataFrame(columns=[v for _, v in mapper.items()])
     for k, v in mapper.items():
         result[v] = df[k]
     return result
