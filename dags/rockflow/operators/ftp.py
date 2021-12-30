78ecee3b979ae5bd5f83858736780e6b0efcda2d
junkai dai <daijunkai@corp.rockflow.ai>
2021-12-30 19:08:00 +0800

    add cocurrent

--- dags/rockflow/operators/ftp.py
+++ dags/rockflow/operators/ftp.py
@@ -1,4 +1,5 @@
 import os
+from multiprocessing.pool import ThreadPool as Pool
 from tempfile import NamedTemporaryFile
 from typing import Any
 
@@ -19,18 +20,27 @@ class SftpToOssOperator(OSSOperator):
         self.prefix = prefix
         self.work_dir = work_dir
         self.ssh_conn_id = ssh_conn_id
+        self.pool_size = 5
         self.ssh_hook = SSHHook(ssh_conn_id=self.ssh_conn_id)
 
     @property
     def sftp_client(self):
         return self.ssh_hook.get_conn().open_sftp()
 
+    def sync_one(self, file):
+        filename = file.filename
+        with NamedTemporaryFile("w") as f:
+            self.sftp_client.get(
+                os.path.join(self.work_dir, filename),
+                f.name
+            )
+            self.put_object_from_file(
+                os.path.join(self.prefix, filename),
+                f.name
+            )
+
     def execute(self, context: Any):
-        for file in self.sftp_client.listdir_attr(self.work_dir):
-            filename = file.filename
-            print(filename)
-            sftp_path = os.path.join(self.work_dir, filename)
-            dest_path = os.path.join(self.prefix, filename)
-            with NamedTemporaryFile("w") as f:
-                self.sftp_client.get(sftp_path, f.name)
-                self.put_object_from_file(dest_path, f.name)
+        with Pool(self.pool_size) as pool:
+            pool.map(
+                lambda file: self.sync_one(file), self.sftp_client.listdir_attr(self.work_dir)
+            )
