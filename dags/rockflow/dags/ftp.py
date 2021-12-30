1d4fd05aebeca5dbf29a3ef4c6cf96867c36dcac
junkai dai <daijunkai@corp.rockflow.ai>
2021-12-30 18:16:38 +0800

    fix dir

--- dags/rockflow/dags/ftp.py
+++ dags/rockflow/dags/ftp.py
@@ -6,7 +6,7 @@ from rockflow.operators.ftp import FtptToOssOperator
 with DAG("ice_sftp_sync", default_args=DEFAULT_DEBUG_ARGS) as ice_sftp_sync:
     FtptToOssOperator(
         prefix=ice_sftp_sync.dag_id,
-        work_dir="/FLOWAIHKFTPH1",
+        work_dir="FLOWAIHKFTPH1",
         ftp_conn_id="ftp_ice",
         region=DEFAULT_REGION,
         bucket_name=DEFAULT_BUCKET_NAME,
