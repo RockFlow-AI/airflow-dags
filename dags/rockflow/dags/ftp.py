eebe041b51d17b41cb8a51197470d21f886f51e7
junkai dai <daijunkai@corp.rockflow.ai>
2021-12-30 18:30:03 +0800

    test sftp

--- dags/rockflow/dags/ftp.py
+++ dags/rockflow/dags/ftp.py
@@ -1,13 +1,13 @@
 from airflow.models import DAG
 
 from rockflow.dags.const import *
-from rockflow.operators.ftp import FtptToOssOperator
+from rockflow.operators.ftp import SftptToOssOperator
 
 with DAG("ice_sftp_sync", default_args=DEFAULT_DEBUG_ARGS) as ice_sftp_sync:
-    FtptToOssOperator(
+    SftptToOssOperator(
         prefix=ice_sftp_sync.dag_id,
-        work_dir="FLOWAIHKFTPH1",
-        ftp_conn_id="ftp_ice",
+        work_dir="/FLOWAIHKFTPH1",
+        ssh_conn_id="ftp_ice",
         region=DEFAULT_REGION,
         bucket_name=DEFAULT_BUCKET_NAME,
         proxy=DEFAULT_PROXY
--- dags/rockflow/operators/ftp.py
+++ dags/rockflow/operators/ftp.py
@@ -1,26 +1,30 @@
+import os
 from typing import Any
 
-from airflow.providers.ftp.hooks.ftp import FTPHook
+from airflow.providers.ssh.hooks.ssh import SSHHook
 
 from rockflow.operators.oss import OSSOperator
 
 
-class FtptToOssOperator(OSSOperator):
+class SftptToOssOperator(OSSOperator):
     def __init__(
             self,
             prefix: str,
             work_dir: str,
-            ftp_conn_id: str = 'ftp_default',
+            ssh_conn_id: str = 'ssh_default',
             **kwargs,
     ) -> None:
         super().__init__(**kwargs)
         self.prefix = prefix
         self.work_dir = work_dir
-        self.ftp_conn_id = ftp_conn_id
-        self.ftp_hook = FTPHook(ftp_conn_id=self.ftp_conn_id)
+        self.ssh_conn_id = ssh_conn_id
+        self.ssh_hook = SSHHook(ssh_conn_id=self.ssh_conn_id)
 
-    def list_directory(self):
-        self.ftp_hook.list_directory(self.work_dir)
+    @property
+    def sftp_client(self):
+        return self.ssh_hook.get_conn().open_sftp()
 
     def execute(self, context: Any):
-        print(self.list_directory())
+        sftp_path = os.path.join(self.work_dir, "K16D75_20211229.SRV")
+        with self.sftp_client.file(sftp_path, mode='rb') as data:
+            self.put_object("ice_sftp_sync/bak", data)
