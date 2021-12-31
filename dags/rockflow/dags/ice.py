2343931013862ba2c7558f2098acd1685889795f
junkai dai <daijunkai@corp.rockflow.ai>
2021-12-31 14:21:53 +0800

    fix sync history

--- dags/rockflow/dags/ice.py
+++ dags/rockflow/dags/ice.py
@@ -4,16 +4,20 @@ from rockflow.dags.const import *
 from rockflow.operators.ice import DailyHistoryImportOperator
 from rockflow.operators.sftp import SftpToOssOperator
 
+
+def get_date_time():
+    return datetime.today().strftime("%Y%m%d")
+
+
 with DAG("ice_sftp_sync", default_args=DEFAULT_DEBUG_ARGS) as ice_sftp_sync:
-    SftpToOssOperator(
+    sync_all_files = SftpToOssOperator(
         prefix=ice_sftp_sync.dag_id,
         work_dir="/FLOWAIHKFTPH1",
         ssh_conn_id="ftp_ice"
     )
 
     daily_history = DailyHistoryImportOperator(
-        task_id='summary_detail_mysql_us',
-        oss_source_key="ice_sftp_sync/K16D75_20211227.SRV",
+        oss_source_key=f"{ice_sftp_sync.dag_id}/K16D75_{get_date_time()}.SRV",
         mysql_table='flow_ticker_stock_price_daily',
         mysql_conn_id=MYSQL_CONNECTION_FLOW_TICKER
     )
--- dags/rockflow/operators/ice.py
+++ dags/rockflow/operators/ice.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from typing import Optional
 
 import pandas as pd
@@ -5,26 +6,58 @@ import pandas as pd
 from rockflow.common.pandas_helper import map_frame
 from rockflow.operators.mysql import OssToMysqlOperator
 
+SYMBOL_COLUMN = "Ticker Code (AS19)"
+EXCHANGE_COLUMN = "Exchange of Quotation (-F3)"
+NASDAQ_EXCHANGE = "NASDAQ"
+HKEX_EXCHANGE = "S.E. HONG KONG LTD (HKEX)"
+DATE_COLUMN = "Price Change Date - Subset 1 (\"FA1)"
+
 
 class DailyHistoryImportOperator(OssToMysqlOperator):
     def __init__(self, **kwargs) -> None:
         if 'index_col' not in kwargs:
-            kwargs['index_col'] = "symbol"
+            kwargs['index_col'] = "id"
         if 'mapping' not in kwargs:
             kwargs['mapping'] = {
-                "Ticker Code (AS19)": "symbol",
-                "Price Change Date - Subset 1 (\"FA1)": "begin",
+                SYMBOL_COLUMN: "symbol",
+                DATE_COLUMN: "begin",
                 "Opening Trade Price Subset-7 ($FS28)": "open",
                 "Price B Sub-Set 3 ($FS3)": "high",
                 "Price A Sub-Set 3 ($FS2)": "low",
                 "Price A Sub-Set 1 ($F4)": "close",
                 "Actual Volume (|FS26)": "volume",
+                "id": "id",
             }
         super().__init__(**kwargs)
 
     def transform(self, df: Optional[pd.DataFrame]) -> Optional[pd.DataFrame]:
-        self.log.info(f"{df[:10]}")
+        def formart_symbol(row):
+            symbol = row[SYMBOL_COLUMN]
+            if row[EXCHANGE_COLUMN] == HKEX_EXCHANGE:
+                if len(symbol) > 0:
+                    return "%05d.HK" % int(symbol)
+            return symbol
+
+        def format_date(v):
+            return datetime.strptime(v, "%Y%m%d").strftime("%Y-%m-%d")
+
+        def format_timestamp(v):
+            return int(datetime.strptime(v, "%Y-%m-%d").timestamp() * 1000)
+
+        def format_id(row):
+            return "%s|%d" % (row[SYMBOL_COLUMN], format_timestamp(row[DATE_COLUMN]))
+
+        df = df.apply(lambda row: row.str.strip(), axis=1)
+        df = df[(df[SYMBOL_COLUMN].notnull()) &
+                (df[SYMBOL_COLUMN] != '') &
+                (df[DATE_COLUMN].notnull()) &
+                (df[DATE_COLUMN] != '') &
+                ((df[EXCHANGE_COLUMN] != NASDAQ_EXCHANGE) | (df[EXCHANGE_COLUMN] == HKEX_EXCHANGE))]
+        df[SYMBOL_COLUMN] = df.apply(lambda row: formart_symbol(row), axis=1)
+        df[DATE_COLUMN] = df[DATE_COLUMN].apply(lambda row: format_date(row))
+        df['id'] = df.apply(lambda row: format_id(row), axis=1)
+        print(df)
         result = map_frame(df, self.mapping)
-        self.log.info(f"{result[:10]}")
+        print(result)
         result.set_index(self.index_col, inplace=True)
         return result
