82f649811cdc3a2e696b2d22053f05d02ecd2ddb
junkai dai <daijunkai@corp.rockflow.ai>
2022-01-04 18:14:01 +0800

    rm histroty debug

--- dags/rockflow/dags/history.py
+++ dags/rockflow/dags/history.py
@@ -18,16 +18,15 @@ with DAG("history_sync", default_args=DEFAULT_DEBUG_ARGS) as history_sync:
         mysql_conn_id=MYSQL_CONNECTION_FLOW_TICKER
     )
 
-with DAG("history_sync_debug", default_args=DEFAULT_DEBUG_ARGS) as history_sync_debug:
     hkex_sync_debug = HistoryImportOperatorDebug(
-        task_id='sync_hkex_history_to_mysql',
+        task_id='sync_hkex_history_to_mysql_debug',
         prefix="day/hkex/",
         mysql_table='flow_ticker_stock_price_daily',
         mysql_conn_id=MYSQL_CONNECTION_FLOW_TICKER
     )
 
     us_sync_debug = HistoryImportOperatorDebug(
-        task_id='sync_us_history_to_mysql',
+        task_id='sync_us_history_to_mysql_debug',
         prefix="day/us/",
         mysql_table='flow_ticker_stock_price_daily',
         mysql_conn_id=MYSQL_CONNECTION_FLOW_TICKER
