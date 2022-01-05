b80082536924e469e64d07c305ce4bbbb0e70009
junkai dai <daijunkai@corp.rockflow.ai>
2022-01-05 17:34:58 +0800

    up dag name

--- dags/rockflow/dags/cluster.py
+++ dags/rockflow/dags/cluster.py
@@ -5,7 +5,7 @@ from rockflow.dags.const import *
 
 # 汇率更新
 currencies_refresh = DAG(
-    "currencies_refresh",
+    "currencies_refresh_by_hour",
     default_args={
         "owner": "yinxiang",
         "start_date": datetime(2022, 1, 5, 0, 0),
@@ -27,7 +27,7 @@ SimpleHttpOperator(
 
 # 碎股及港股更新
 contracts_refresh = DAG(
-    "contracts_refresh",
+    "contracts_refresh_daily",
     default_args={
         "owner": "yinxiang",
         "start_date": datetime(2022, 1, 5, 1, 0),
@@ -49,7 +49,7 @@ SimpleHttpOperator(
 
 # 实时行情聚合为1分钟
 ticks = DAG(
-    "ticks",
+    "ticks_by_minute",
     default_args={
         "owner": "yinxiang",
         "start_date": datetime(2022, 1, 5, 1, 0),
