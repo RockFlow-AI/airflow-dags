17d6941f33cce408ba69b1d1138878d2c4a83b75
maoboxuan <maoboxuan@flowcapai.com>
2022-04-12 20:45:26 +0800

    update time

--- dags/rockflow/dags/cluster.py
+++ dags/rockflow/dags/cluster.py
@@ -393,7 +393,7 @@ daily_statement = DAG(
     "daily_statement",
     catchup=False,
     start_date=pendulum.datetime(2022, 3, 10, tz='Asia/Hong_Kong'),
-    schedule_interval='0 0 16 * * ?',
+    schedule_interval='0 0 16 * *',
     default_args={
         "owner": "maoboxuan",
         "depends_on_past": False,
@@ -486,7 +486,7 @@ monthly_statement = DAG(
     "monthly_statement",
     catchup=False,
     start_date=pendulum.datetime(2022, 3, 10, tz='Asia/Hong_Kong'),
-    schedule_interval='0 0 16 2 * ?',
+    schedule_interval='0 0 16 2 *',
     default_args={
         "owner": "maoboxuan",
         "depends_on_past": False,
