a95495ba4587744aa1a4bcd6083923f3faf02a13
jingjiadong <jingjiadong@flowcapai.com>
2022-05-20 01:35:48 +0800

    add cash_transfer_status_query_update task

--- dags/rockflow/dags/cluster.py
+++ dags/rockflow/dags/cluster.py
@@ -545,6 +545,28 @@ SimpleHttpOperator(
     http_conn_id='flow-account-channel',
     endpoint='/inner/ib/dam/account/status',
     response_check=lambda response: response.json()['code'] == 200,
-    extra_options={"timeout": 1000},
+    extra_options={"timeout": 1200},
     dag=dam_account_status_query,
+)
+
+cash_transfer_status_query_update = DAG(
+    "cash_transfer_status_query_update",
+    catchup=False,
+    start_date=pendulum.datetime(2022, 5, 18),
+    schedule_interval=timedelta(minutes=1),
+    default_args={
+        "owner": "jingjiadong",
+        "depends_on_past": False,
+        "retries": 0,
+    }
+)
+
+SimpleHttpOperator(
+    task_id='cash_transfer_status_query_update',
+    method='POST',
+    http_conn_id='flow-account-channel',
+    endpoint='/inner/ib/dam/cash-transfer/status',
+    response_check=lambda response: response.json()['code'] == 200,
+    extra_options={"timeout": 1000},
+    dag=cash_transfer_status_query_update,
 )
