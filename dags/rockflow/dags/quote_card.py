a6e356c383aea65c418abdf1d8a7f948579cbb86
hujing <hujing@flowcapai.com>
2022-05-25 17:01:56 +0800

    quote card renew

---
+++ dags/rockflow/dags/quote_card.py
@@ -0,0 +1,29 @@
+import time
+import pendulum
+from datetime import datetime, timedelta
+
+from airflow.models import DAG
+from airflow.providers.http.operators.http import SimpleHttpOperator
+
+# 行情卡续订
+quote_card_renew = DAG(
+    "quote_card_renew",
+    catchup=False,
+    start_date=pendulum.datetime(2022, 7, 1),
+    schedule_interval='0 */2 1,2 * *',
+    default_args={
+        "owner": "hujing",
+        "depends_on_past": False,
+        "retries": 0,
+    }
+)
+
+SimpleHttpOperator(
+    task_id='quote_card_renew',
+    method='POST',
+    http_conn_id='flow-shop',
+    endpoint='/shop/inner/quoteCard/renew',
+    response_check=lambda response: response.json()['code'] == 200,
+    extra_options={"timeout": 60},
+    dag=quote_card_renew,
+)
