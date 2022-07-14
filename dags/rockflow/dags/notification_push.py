a03c6ace8392ef4d3e86270bd7c70e5f483559e0
jingjiadong <jingjiadong@flowcapai.com>
2022-07-14 15:17:28 +0800

    add HALA push notification

---
+++ dags/rockflow/dags/notification_push.py
@@ -0,0 +1,27 @@
+import pendulum
+
+from airflow.models import DAG
+from airflow.providers.http.operators.http import SimpleHttpOperator
+
+
+DAILY_HALA_ALL_SENDING = DAG(
+    "DAILY_HALA_ALL_SENDING",
+    catchup=False,
+    start_date=pendulum.datetime(2022, 7, 8, tz='Asia/Shanghai'),
+    schedule_interval='0 22 * * *',
+    default_args={
+        "owner": "jingjiadong",
+        "depends_on_past": False,
+        "retries": 0,
+    }
+)
+
+SimpleHttpOperator(
+    task_id='DAILY_HALA_ALL_SENDING',
+    method='POST',
+    http_conn_id='flow-notification',
+    endpoint='/inner/kline/ld?step=2&timeframe=1d&adjustment=forward&markets=US',
+    response_check=lambda response: response.json()['code'] == 200,
+    extra_options={"timeout": 3600},
+    dag=DAILY_HALA_ALL_SENDING,
+)
