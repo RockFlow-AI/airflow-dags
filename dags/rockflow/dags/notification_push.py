c0d0537c1b8fc3f246da343c12dfde3239cb6ea3
jingjiadong <jingjiadong@flowcapai.com>
2022-07-18 21:12:51 +0800

    add us stock market open notification

--- dags/rockflow/dags/notification_push.py
+++ dags/rockflow/dags/notification_push.py
@@ -25,3 +25,25 @@ SimpleHttpOperator(
     extra_options={"timeout": 3600},
     dag=DAILY_HALA_ALL_SENDING,
 )
+
+US_MARKET_OPEN_NOTIFICATION = DAG(
+    "US_MARKET_OPEN_NOTIFICATION",
+    catchup=False,
+    start_date=pendulum.datetime(2022, 7, 18, tz='America/New_York'),
+    schedule_interval='29 09 * * *',
+    default_args={
+        "owner": "jingjiadong",
+        "depends_on_past": False,
+        "retries": 0,
+    }
+)
+
+SimpleHttpOperator(
+    task_id='US_MARKET_OPEN_NOTIFICATION',
+    method='POST',
+    http_conn_id='flow-ticker-service',
+    endpoint='/ticker/inner/tradingHours/open/notification/task?market=US',
+    response_check=lambda response: response.json()['code'] == 200,
+    extra_options={"timeout": 3600},
+    dag=US_MARKET_OPEN_NOTIFICATION,
+)
