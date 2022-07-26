2a21ae2c3a5595fc238a556d5a4ee10725fefa49
yinxiang <yinxiang@flowcapai.com>
2022-07-26 18:01:59 +0800

    single dag for multiple shards

--- dags/rockflow/dags/option_algorithm.py
+++ dags/rockflow/dags/option_algorithm.py
@@ -47,52 +47,48 @@ def option_algorithm_task(dag, shard: int) -> PythonOperator:
     )
 
 
-def option_dag_shard(shard: int):
-    with DAG(
-            f"option_algorithm_us_09-30_09-50_{shard}",
-            catchup=False,
-            start_date=pendulum.datetime(2022, 7, 22, tz='America/New_York'),
-            schedule_interval='30-50/10 09 * * 1-5',
-            concurrency=1,
-            default_args={
-                "owner": "yinxiang",
-                "depends_on_past": False,
-                "retries": 0,
-            }
-    ) as dag_us_1:
-        option_algorithm_task(dag_us_1, shard)
-        globals()[f'option_dag_us_1_{shard}'] = dag_us_1
+with DAG(
+        f"option_algorithm_us_09-30_09-50",
+        catchup=False,
+        start_date=pendulum.datetime(2022, 7, 22, tz='America/New_York'),
+        schedule_interval='30-50/10 09 * * 1-5',
+        concurrency=1,
+        default_args={
+            "owner": "yinxiang",
+            "depends_on_past": False,
+            "retries": 0,
+        }
+) as dag_us_1:
+    for i in range(OPTION_ALGO_SHARDS):
+        option_algorithm_task(dag_us_1, i)
 
-    with DAG(
-            f"option_algorithm_us_10-00_19-50_{shard}",
-            catchup=False,
-            start_date=pendulum.datetime(2022, 7, 22, tz='America/New_York'),
-            schedule_interval='*/10 10-19 * * 1-5',
-            concurrency=1,
-            default_args={
-                "owner": "yinxiang",
-                "depends_on_past": False,
-                "retries": 0,
-            }
-    ) as dag_us_2:
-        option_algorithm_task(dag_us_2, shard)
-        globals()[f'option_dag_us_2_{shard}'] = dag_us_2
+with DAG(
+        f"option_algorithm_us_10-00_19-50",
+        catchup=False,
+        start_date=pendulum.datetime(2022, 7, 22, tz='America/New_York'),
+        schedule_interval='*/10 10-19 * * 1-5',
+        concurrency=1,
+        default_args={
+            "owner": "yinxiang",
+            "depends_on_past": False,
+            "retries": 0,
+        }
+) as dag_us_2:
+    for i in range(OPTION_ALGO_SHARDS):
+        option_algorithm_task(dag_us_2, i)
 
-    with DAG(
-            f"option_algorithm_us_20-00_{shard}",
-            catchup=False,
-            start_date=pendulum.datetime(2022, 7, 22, tz='America/New_York'),
-            schedule_interval='0 20 * * 1-5',
-            concurrency=1,
-            default_args={
-                "owner": "yinxiang",
-                "depends_on_past": False,
-                "retries": 0,
-            }
-    ) as dag_us_3:
-        option_algorithm_task(dag_us_3, shard)
-        globals()[f'option_dag_us_3_{shard}'] = dag_us_3
+with DAG(
+        f"option_algorithm_us_20-00",
+        catchup=False,
+        start_date=pendulum.datetime(2022, 7, 22, tz='America/New_York'),
+        schedule_interval='0 20 * * 1-5',
+        concurrency=1,
+        default_args={
+            "owner": "yinxiang",
+            "depends_on_past": False,
+            "retries": 0,
+        }
+) as dag_us_3:
+    for i in range(OPTION_ALGO_SHARDS):
+        option_algorithm_task(dag_us_3, i)
 
-
-for i in range(OPTION_ALGO_SHARDS):
-    option_dag_shard(i)
