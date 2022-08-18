b9bb12942d20ce61efd32948ad71410f6dfe0bb9
yinxiang <yinxiang@flowcapai.com>
2022-08-18 14:02:50 +0800

    update option chain on sunday only

--- dags/rockflow/dags/symbol_option.py
+++ dags/rockflow/dags/symbol_option.py
@@ -110,7 +110,7 @@ option_chain_us = DAG(
     "option_chain_us",
     catchup=False,
     start_date=pendulum.datetime(2022, 2, 28, tz='America/New_York'),
-    schedule_interval='0 3 * * 1-5',
+    schedule_interval='0 15 * * 0',
     default_args={
         "owner": "yinxiang",
         "depends_on_past": False,
