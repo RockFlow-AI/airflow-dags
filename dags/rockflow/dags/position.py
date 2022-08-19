05885ce459d7c9069b7375c95be4b3cd99f3009c
yinxiang <yinxiang@flowcapai.com>
2022-08-19 14:32:06 +0800

    overwrite position estimates

---
+++ dags/rockflow/dags/position.py
@@ -0,0 +1,30 @@
+from datetime import timedelta
+
+import pendulum
+from airflow.models import DAG
+from airflow.providers.http.operators.http import SimpleHttpOperator
+from rockflow.dags.const import *
+from rockflow.operators.symbol import *
+
+position_estimate_overwrite = DAG(
+    "position_estimate_overwrite",
+    catchup=False,
+    start_date=pendulum.datetime(2022, 8, 19, tz='America/New_York'),
+    schedule_interval='10 20 * * 1-5',
+    default_args={
+        "owner": "yinxiang",
+        "depends_on_past": False,
+        "retries": 5,
+        "retry_delay": timedelta(minutes=5),
+    }
+)
+
+position_estimate_overwrite_task = SimpleHttpOperator(
+    task_id='position_estimate_overwrite',
+    method='PATCH',
+    http_conn_id='flow-portfolio-service',
+    endpoint='/portfolio/inner/positions',
+    response_check=lambda response: response.json()['code'] == 200,
+    extra_options={"timeout": 60},
+    dag=position_estimate_overwrite,
+)
