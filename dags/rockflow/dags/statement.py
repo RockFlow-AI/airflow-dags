660a34013eec58c7cf1781a6c2d7bc8c821e91d3
caoyunfei <caoyunfei@flowcapai.com>
2022-10-08 11:30:37 +0800

    add statement sync task

---
+++ dags/rockflow/dags/statement.py
@@ -0,0 +1,26 @@
+from airflow.models import DAG
+from datetime import datetime
+from airflow.providers.http.operators.http import SimpleHttpOperator
+
+
+statements_daily = DAG(
+    "ledger_statement_by_daily",
+    catchup=False,
+    start_date=datetime(2022, 1, 5, 0, 0),
+    schedule_interval='0 0 8 * * ?',
+    default_args={
+        "owner": "caoyunfei",
+        "depends_on_past": False,
+        "retries": 0,
+    }
+)
+
+statements_on_time = SimpleHttpOperator(
+    task_id='statements_daily',
+    method='PATCH',
+    http_conn_id='flow-ledger',
+    endpoint='/ledger/inner/statement/daily',
+    response_check=lambda response: response.json()['code'] == 200,
+    extra_options={"timeout": 60},
+    dag=statements_daily,
+)
