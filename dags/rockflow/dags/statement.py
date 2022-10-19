2df69928aa2573270c8761409e313008c3c1c16b
caoyunfei <caoyunfei@flowcapai.com>
2022-10-19 12:59:32 +0800

    add statement service task

--- dags/rockflow/dags/statement.py
+++ dags/rockflow/dags/statement.py
@@ -7,7 +7,7 @@ ledger_statement_by_daily = DAG(
     "ledger_statement_by_daily",
     catchup=False,
     start_date=datetime(2022, 10, 13, 0, 0),
-    schedule_interval='0 0 8 * * *',
+    schedule_interval='0 30 8 * * *',
     default_args={
         "owner": "caoyunfei",
         "depends_on_past": False,
@@ -21,6 +21,16 @@ SimpleHttpOperator(
     method='PATCH',
     http_conn_id='flow-ledger',
     endpoint='/ledger/inner/statement/daily',
+    response_check=lambda response: response.json()['code'] == 200,
+    extra_options={"timeout": 60},
+    dag=ledger_statement_by_daily,
+)
+
+SimpleHttpOperator(
+    task_id='statement_new_by_daily',
+    method='PATCH',
+    http_conn_id='flow-statement',
+    endpoint='/inner/statements/daily',
     response_check=lambda response: response.json()['code'] == 200,
     extra_options={"timeout": 60},
     dag=ledger_statement_by_daily,
