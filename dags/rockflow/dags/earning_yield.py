9fd3ab4f7e7d81dec9c1ef01529e6da80a4b214f
caoyunfei <caoyunfei@flowcapai.com>
2022-11-04 16:38:20 +0800

    fix earning yield task

--- dags/rockflow/dags/earning_yield.py
+++ dags/rockflow/dags/earning_yield.py
@@ -4,8 +4,8 @@ from datetime import datetime, timedelta
 from airflow.providers.http.operators.http import SimpleHttpOperator
 
 # 收益率排行榜_1d
-earningYield_leaderboard_update_1d = DAG(
-    "rock_star_accumulate_days",
+earning_yield_leaderboard_update_1d = DAG(
+    "earning_yield_leaderboard_update_1d",
     catchup=False,
     start_date=pendulum.datetime(2022, 11, 4, tz='America/New_York'),
     schedule_interval='32 20 * * 1-5',
@@ -18,18 +18,18 @@ earningYield_leaderboard_update_1d = DAG(
 )
 
 SimpleHttpOperator(
-    task_id='earningYield_leaderboard_update_1d',
+    task_id='earning_yield_leaderboard_update_1d',
     method='PATCH',
     http_conn_id='flow-social',
     endpoint='/social/inner/earningYield/leaderboards/update/1d',
     response_check=lambda response: response.json()['code'] == 200,
     extra_options={"timeout": 60},
-    dag=earningYield_leaderboard_update_1d,
+    dag=earning_yield_leaderboard_update_1d,
 )
 
 # 收益率排行榜_10min
-earningYield_leaderboard_update_10m = DAG(
-    "rock_star_accumulate_days",
+earning_yield_leaderboard_update_10m = DAG(
+    "earning_yield_leaderboard_update_10m",
     catchup=False,
     start_date=pendulum.datetime(2022, 11, 4, tz='America/New_York'),
     schedule_interval='32/10 20 * * 1-5',
@@ -42,12 +42,12 @@ earningYield_leaderboard_update_10m = DAG(
 )
 
 SimpleHttpOperator(
-    task_id='earningYield_leaderboard_update_10m',
+    task_id='earning_yield_leaderboard_update_10m',
     method='PATCH',
     http_conn_id='flow-social',
     endpoint='/social/inner/earningYield/leaderboards/update/10m',
     response_check=lambda response: response.json()['code'] == 200,
     extra_options={"timeout": 60},
-    dag=earningYield_leaderboard_update_10m,
+    dag=earning_yield_leaderboard_update_10m,
 )
 
