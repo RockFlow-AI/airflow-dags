90f79f27d2b8cb541ddf223dea5632474a9ee82a
caoyunfei <caoyunfei@flowcapai.com>
2022-11-04 14:06:53 +0800

    add rock_star_accumulate_days

---
+++ dags/rockflow/dags/rockStar.py
@@ -0,0 +1,29 @@
+from airflow.models import DAG
+from datetime import datetime, timedelta
+from airflow.providers.http.operators.http import SimpleHttpOperator
+
+
+rock_star_accumulate_days = DAG(
+    "rock_star_accumulate_days",
+    catchup=False,
+    start_date=pendulum.datetime(2022, 11, 4, tz='America/New_York'),
+    schedule_interval='35 20 * * 1-5',
+    default_args={
+        "owner": "caoyunfei",
+        "depends_on_past": False,
+        "retries": 0,
+    }
+)
+
+SimpleHttpOperator(
+    task_id='RockStarAccumulateDays',
+    method='GET',
+    http_conn_id='flow-social',
+    endpoint='/social/inner/earningYield/rockerStar/daily',
+    response_check=lambda response: response.json()['code'] == 200,
+    extra_options={"timeout": 60},
+    dag="rock_star_accumulate_days",
+)
+
+
+
