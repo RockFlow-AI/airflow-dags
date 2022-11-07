c21f087d571303dc51890c573ffe49fef5a6eb1a
yinxiang <yinxiang@flowcapai.com>
2022-11-07 23:06:16 +0800

    Revert "remove watchlist dag"
    This reverts commit 34bed5369b358fcc3951b9817e056afe0f6fcdda.

--- dags/rockflow/dags/watchlist.py
+++ dags/rockflow/dags/watchlist.py
@@ -27,4 +27,26 @@ SimpleHttpOperator(
     dag=WATCHLIST_US_DAY,
 )
 
+WATCHLIST_US_MINUTE = DAG(
+    "WATCHLIST_US_MINUTE",
+    catchup=False,
+    start_date=pendulum.datetime(2022, 9, 8, tz='America/New_York'),
+    schedule_interval='*/10 9-17 * * 1-5',
+    default_args={
+        "owner": "chenborui",
+        "depends_on_past": False,
+        "retries": 0,
+    }
+)
+
+SimpleHttpOperator(
+    task_id='WATCHLIST_US_MINUTE',
+    method='PUT',
+    http_conn_id='flow-mr-option',
+    endpoint='/watchlist/inner/tasks',
+    data={"period": "MINUTE"},
+    response_check=lambda response: response.json()['code'] == 200,
+    extra_options={"timeout": 3600},
+    dag=WATCHLIST_US_MINUTE,
+)
 
