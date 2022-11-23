009023e8145ecc26649863fffa5c43c82dab7b9f
caoyunfei <caoyunfei@flowcapai.com>
2022-11-23 10:46:56 +0800

    add interest

---
+++ dags/rockflow/dags/interest.py
@@ -0,0 +1,33 @@
+import pytz
+from airflow.models import DAG
+from datetime import datetime
+from airflow.providers.http.operators.http import SimpleHttpOperator
+
+task_time = int(round(datetime
+                      .now(tz=pytz.timezone('UTC'))
+                      .replace(hour=8, minute=0, second=0,microsecond=0)
+                      .timestamp() * 1000))
+
+interest_path = '/ledger/inner/interest/task?time={task_time}'.format(task_time=task_time)
+
+ledger_interest_daily_calculator = DAG(
+    "ledger_interest_daily_calculator",
+    catchup=False,
+    start_date=datetime(2022, 10, 19, 0, 0),
+    schedule_interval='0 8 * * 1-7',
+    default_args={
+        "owner": "caoyunfei",
+        "depends_on_past": False,
+        "retries": 0
+    }
+)
+
+SimpleHttpOperator(
+    task_id='ledger_interest_daily_calculator',
+    method='POST',
+    http_conn_id='flow-ledger',
+    endpoint=interest_path,
+    response_check=lambda response: response.json()['code'] == 200,
+    extra_options={"timeout": 60},
+    dag=ledger_interest_daily_calculator,
+)
--- dags/rockflow/dags/statement.py
+++ dags/rockflow/dags/statement.py
@@ -7,7 +7,7 @@ ledger_statement_by_daily = DAG(
     "ledger_statement_by_daily",
     catchup=False,
     start_date=datetime(2022, 10, 13, 0, 0),
-    schedule_interval='0 0 8 * * *',
+    schedule_interval='0 30 8 * * ?',
     default_args={
         "owner": "caoyunfei",
         "depends_on_past": False,
@@ -24,4 +24,27 @@ SimpleHttpOperator(
     response_check=lambda response: response.json()['code'] == 200,
     extra_options={"timeout": 60},
     dag=ledger_statement_by_daily,
+)
+
+statement_new_by_daily = DAG(
+    "statement_new_by_daily",
+    catchup=False,
+    start_date=datetime(2022, 10, 13, 0, 0),
+    schedule_interval='30 8 * * 1-7',
+    default_args={
+        "owner": "caoyunfei",
+        "depends_on_past": False,
+        "retries": 16,
+        "retry_delay": timedelta(minutes=30)
+    }
+)
+
+SimpleHttpOperator(
+    task_id='statement_new_by_daily',
+    method='PATCH',
+    http_conn_id='flow-statement',
+    endpoint='/inner/statements/daily',
+    response_check=lambda response: response.json()['code'] == 200,
+    extra_options={"timeout": 60},
+    dag=statement_new_by_daily,
 )
