dea250eb4197ecde47790ca1920d4d445f52eed5
caoyunfei <caoyunfei@flowcapai.com>
2022-12-22 16:56:08 +0800

    add sync ftp and option exercise

--- dags/rockflow/dags/statement.py
+++ dags/rockflow/dags/statement.py
@@ -48,3 +48,48 @@ SimpleHttpOperator(
     extra_options={"timeout": 60},
     dag=statement_new_by_daily,
 )
+
+statement_sync_ftp_file = DAG(
+    "statement_sync_ftp_file",
+    catchup=False,
+    start_date=datetime(2022, 10, 22, 0, 0),
+    schedule_interval='30 7 * * 1-7',
+    default_args={
+        "owner": "chenborui",
+        "depends_on_past": False,
+        "retries": 3,
+        "retry_delay": timedelta(minutes=30)
+    }
+)
+
+SimpleHttpOperator(
+    task_id='statement_sync_ftp_file',
+    method='PATCH',
+    http_conn_id='flow-statement',
+    endpoint='/inner/statement/ftpFiles/sync?date={date}'.format(date=datetime.now().strftime("%Y%m%d")),
+    response_check=lambda response: response.json()['code'] == 200,
+    extra_options={"timeout": 600},
+    dag=statement_sync_ftp_file,
+)
+
+option_exercise_report = DAG(
+    "option_exercise_report",
+    catchup=False,
+    start_date=datetime(2022, 12, 5, 0, 0),
+    schedule_interval='30 9 * * 1-7',
+    default_args={
+        "owner": "caoyunfei",
+        "depends_on_past": False,
+        "retries": 0
+    }
+)
+
+SimpleHttpOperator(
+    task_id='option_exercise_report',
+    method='PATCH',
+    http_conn_id='flow-statement',
+    endpoint='/inner/option/exercise/send',
+    response_check=lambda response: response.json()['code'] == 200,
+    extra_options={"timeout": 60},
+    dag=option_exercise_report,
+)
