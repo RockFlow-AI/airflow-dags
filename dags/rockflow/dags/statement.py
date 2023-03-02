690c441f5f51edb780a1bd00a8df8d61c853d402
caoyunfei <caoyunfei@flowcapai.com>
2023-03-02 17:03:22 +0800

    divide route alert to margin and debt task

--- dags/rockflow/dags/statement.py
+++ dags/rockflow/dags/statement.py
@@ -189,7 +189,7 @@ risk_debt_report = DAG(
     "risk_debt_report",
     catchup=False,
     start_date=datetime(2023, 2, 21, 0, 0),
-    schedule_interval='00 10 * * 1-7',
+    schedule_interval='30 1 * * 1-7',
     default_args={
         "owner": "caoyunfei",
         "depends_on_past": False,
@@ -201,10 +201,32 @@ SimpleHttpOperator(
     task_id='risk_debt_report',
     method='PATCH',
     http_conn_id='flow-statement',
-    endpoint='/inner/risk/debt/send?date={date}'.format(date=datetime.now().strftime("%Y%m%d")),
+    endpoint='/inner/risk/debt/send?date={date}'.format(date=(datetime.now() + timedelta(days=-1)).strftime("%Y%m%d")),
     response_check=lambda response: response.json()['code'] == 200,
     extra_options={"timeout": 60},
     dag=risk_debt_report,
+)
+
+risk_margin_report = DAG(
+    "risk_margin_report",
+    catchup=False,
+    start_date=datetime(2023, 2, 21, 0, 0),
+    schedule_interval='00 10 * * 1-7',
+    default_args={
+        "owner": "caoyunfei",
+        "depends_on_past": False,
+        "retries": 0
+    }
+)
+
+SimpleHttpOperator(
+    task_id='risk_margin_report',
+    method='PATCH',
+    http_conn_id='flow-statement',
+    endpoint='/inner/risk/margin/send?date={date}'.format(date=datetime.now().strftime("%Y%m%d")),
+    response_check=lambda response: response.json()['code'] == 200,
+    extra_options={"timeout": 60},
+    dag=risk_margin_report,
 )
 
 US_trade_match_report = DAG(
