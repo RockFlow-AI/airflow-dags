e8391cdf5612727822cb09d259f950e0d630f48c
jingjiadong <jingjiadong@flowcapai.com>
2023-03-14 12:04:46 +0800

    add hala_2 task

--- dags/rockflow/dags/notification_push.py
+++ dags/rockflow/dags/notification_push.py
@@ -26,6 +26,28 @@ SimpleHttpOperator(
     dag=DAILY_HALA_ALL_SENDING,
 )
 
+DAILY_HALA_2_ALL_SENDING = DAG(
+    "DAILY_HALA_2_ALL_SENDING",
+    catchup=False,
+    start_date=pendulum.datetime(2023, 3, 14, tz='Asia/Shanghai'),
+    schedule_interval='0 15 * * *',
+    default_args={
+        "owner": "jingjiadong",
+        "depends_on_past": False,
+        "retries": 0,
+    }
+)
+
+SimpleHttpOperator(
+    task_id='DAILY_HALA_2_ALL_SENDING',
+    method='POST',
+    http_conn_id='flow-notification',
+    endpoint='/push/inner/task/all/days/HALA_2_',
+    response_check=lambda response: response.json()['code'] == 200,
+    extra_options={"timeout": 3600},
+    dag=DAILY_HALA_2_ALL_SENDING,
+)
+
 
 DAILY_DUSK_ALL_SENDING = DAG(
     "DAILY_DUSK_ALL_SENDING",
