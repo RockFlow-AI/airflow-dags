8740ebc8aec652bbe4c002a6efb00153667f39f0
caoyunfei <caoyunfei@flowcapai.com>
2023-03-16 16:36:35 +0800

    add clear rock star month every month

--- dags/rockflow/dags/rockStar.py
+++ dags/rockflow/dags/rockStar.py
@@ -9,7 +9,7 @@ rock_star_accumulate_days = DAG(
     "rock_star_accumulate_days",
     catchup=False,
     start_date=pendulum.datetime(2022, 11, 4),
-    schedule_interval='1 1 * * *',
+    schedule_interval='*/10 * * * *',
     default_args={
         "owner": "caoyunfei",
         "depends_on_past": False,
@@ -25,6 +25,28 @@ SimpleHttpOperator(
     response_check=lambda response: response.json()['code'] == 200,
     extra_options={"timeout": 60},
     dag=rock_star_accumulate_days,
+)
+
+rock_star_clear_accumulate_month_days = DAG(
+    "rock_star_clear_accumulate_month_days",
+    catchup=False,
+    start_date=pendulum.datetime(2022, 11, 4),
+    schedule_interval='0 0 1 */1 *',
+    default_args={
+        "owner": "caoyunfei",
+        "depends_on_past": False,
+        "retries": 0,
+    }
+)
+
+SimpleHttpOperator(
+    task_id='rock_star_clear_accumulate_month_days',
+    method='PATCH',
+    http_conn_id='flow-social',
+    endpoint='/social/inner/earningYield/rockerStar/daily',
+    response_check=lambda response: response.json()['code'] == 200,
+    extra_options={"timeout": 60},
+    dag=rock_star_clear_accumulate_month_days,
 )
 
 
