ec5b6b2d0edf6b98e0f8c86a2ec134444fb2c229
yinxiang <yinxiang@flowcapai.com>
2023-05-19 11:15:58 +0800

    Revert "fix static method calling"
    This reverts commit 9e86fc3aa517c933504bbffa0f4e19bcb6209f47.

--- dags/rockflow/operators/futu.py
+++ dags/rockflow/operators/futu.py
@@ -186,13 +186,9 @@ class FutuFormatJson(OSSSaveOperator):
         raise NotImplementedError()
 
     @property
-    def instance(self):
-        raise NotImplementedError()
-
-    @property
     def content(self):
         result = [
-            self.instance.format(i)
+            self.cls.format_(self.cls, self.cls.language(), i)
             for i in json.load(
                 BytesIO(self.get_object(self.from_key).read())
             )
@@ -208,10 +204,6 @@ class FutuFormatJsonCn(FutuFormatJson):
     def cls(self):
         return FutuCompanyProfileCn
 
-    @property
-    def instance(self):
-        return FutuCompanyProfileCn()
-
 
 class FutuFormatJsonEn(FutuFormatJson):
     def __init__(self, **kwargs) -> None:
@@ -220,10 +212,6 @@ class FutuFormatJsonEn(FutuFormatJson):
     @property
     def cls(self):
         return FutuCompanyProfileEn
-
-    @property
-    def instance(self):
-        return FutuCompanyProfileEn()
 
 
 class JoinMap(OSSSaveOperator):
