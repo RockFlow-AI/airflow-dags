a90191ff8ef638a43a2773f45499608a8ef826bd
yuzhiqiang​ <yuzhiqiang@flowcapai.com>
2023-06-28 18:37:25 +0800

    asset task

--- dags/rockflow/dags/asset.py
+++ dags/rockflow/dags/asset.py
@@ -8,7 +8,7 @@ nvl_asset = DAG(
     "nvl_asset",
     catchup=False,
     start_date=pendulum.datetime(2023, 6, 28, tz='Asia/Shanghai'),
-    schedule_interval='0 25 18 * * ?',
+    schedule_interval='40 18 * * * *',
     default_args={
         "owner": "yuzhiqiang",
         "depends_on_past": False,
