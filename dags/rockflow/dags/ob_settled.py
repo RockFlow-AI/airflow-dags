fc5f5442c9ed429a5060c5ec0d229da3be52a256
maoboxuan <maoboxuan@flowcapai.com>
2023-06-29 14:38:42 +0800

    add

--- dags/rockflow/dags/ob_settled.py
+++ dags/rockflow/dags/ob_settled.py
@@ -26,3 +26,27 @@ SimpleHttpOperator(
     extra_options={"timeout": 60},
     dag=ob_settled_stat,
 )
+
+
+auto_trade_match = DAG(
+    "auto_trade_match",
+    catchup=False,
+    start_date=pendulum.datetime(2023, 4, 20, tz='UTC'),
+    schedule_interval='30 10 * * 1-7',
+    default_args={
+        "owner": "chengwei",
+        "depends_on_past": False,
+        "retries": 5,
+        "retry_delay": timedelta(minutes=1),
+    }
+)
+
+SimpleHttpOperator(
+    task_id='auto_trade_match',
+    method='POST',
+    http_conn_id='flow-ledger',
+    endpoint='/ledger/inner/tradeMatch/auto',
+    response_check=lambda response: response.json()['code'] == 200,
+    extra_options={"timeout": 60},
+    dag=auto_trade_match,
+)
