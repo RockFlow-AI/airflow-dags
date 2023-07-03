c156c18433b8ea98abffc4c34739080c8abe8502
yuzhiqiang​ <yuzhiqiang@flowcapai.com>
2023-07-03 17:22:25 +0800

    asset task

--- dags/rockflow/dags/asset.py
+++ dags/rockflow/dags/asset.py
@@ -4,8 +4,8 @@ from datetime import timedelta
 from airflow.providers.http.operators.http import SimpleHttpOperator
 
 # 定时任务 - 每天北京时间六点调用一次
-nvl_asset = DAG(
-    "nvl_asset",
+asset = DAG(
+    "asset",
     catchup=False,
     start_date=pendulum.datetime(2023, 6, 28, tz='Asia/Shanghai'),
     schedule_interval='0 18 * * *',
@@ -18,11 +18,11 @@ nvl_asset = DAG(
 )
 
 SimpleHttpOperator(
-    task_id='nvl_asset',
+    task_id='asset',
     method='POST',
     http_conn_id='flow-feed-portfolio',
     endpoint='/account/inner/calculate/asset',
     response_check=lambda response: response.json()['code'] == 200,
     extra_options={"timeout": 60},
-    dag=nvl_asset,
+    dag=asset,
 )
