8a74b7cb043a44ca65b617d18dc2f61740270c74
Esqer <mr.askar@outlook.com>
2023-07-17 18:01:30 +0800

    tradeGPT option update bot

---
+++ dags/rockflow/dags/tradeGPT.py
@@ -0,0 +1,116 @@
+# 神策业务指标报警 - 定时任务
+
+import pendulum
+from airflow.models import DAG
+from datetime import timedelta
+from airflow.providers.http.operators.http import SimpleHttpOperator
+
+tradeGPT_run_initial = DAG(
+    "tradeGPT_run_initial",
+    catchup=False,
+    start_date=pendulum.datetime(2023, 7, 17, tz='Asia/Shanghai'),
+    schedule_interval="0 10 * * 1-5",
+    default_args={
+        "owner": "huangdexi",
+        "depends_on_past": False,
+        "retries": 1,
+        "retry_delay": timedelta(minutes=3)
+    }
+)
+
+SimpleHttpOperator(
+    task_id='tradeGPT_run_initial',
+    method='GET',
+    http_conn_id='tradegpt',
+    endpoint="/initial",
+    response_check=lambda response: response.json()['code'] == 200,
+    dag=tradeGPT_run_initial,
+)
+
+tradeGPT_run_second = DAG(
+    "tradeGPT_run_second",
+    catchup=False,
+    start_date=pendulum.datetime(2023, 7, 17, tz='Asia/Shanghai'),
+    schedule_interval="45 18 * * 1-5",
+    default_args={
+        "owner": "huangdexi",
+        "depends_on_past": False,
+        "retries": 1,
+        "retry_delay": timedelta(minutes=3)
+    }
+)
+
+SimpleHttpOperator(
+    task_id='tradeGPT_run_second',
+    method='GET',
+    http_conn_id='tradegpt',
+    endpoint="/second",
+    response_check=lambda response: response.json()['code'] == 200,
+    dag=tradeGPT_run_second,
+)
+
+tradeGPT_option_update_1 = DAG(
+    "tradeGPT_option_update_1",
+    catchup=False,
+    start_date=pendulum.datetime(2023, 7, 17, tz='Asia/Shanghai'),
+    schedule_interval="2-56/10 21 * * 1-5",
+    default_args={
+        "owner": "huangdexi",
+        "depends_on_past": False,
+        "retries": 1,
+        "retry_delay": timedelta(minutes=3)
+    }
+)
+
+SimpleHttpOperator(
+    task_id='tradeGPT_option_update_1',
+    method='GET',
+    http_conn_id='tradegpt',
+    endpoint="/option_update/run_option_update",
+    response_check=lambda response: response.json()['code'] == 200,
+    dag=tradeGPT_option_update_1,
+)
+
+tradeGPT_option_update_2 = DAG(
+    "tradeGPT_option_update_2",
+    catchup=False,
+    start_date=pendulum.datetime(2023, 7, 17, tz='Asia/Shanghai'),
+    schedule_interval="2-56/30 22-23 * * 1-5",
+    default_args={
+        "owner": "huangdexi",
+        "depends_on_past": False,
+        "retries": 1,
+        "retry_delay": timedelta(minutes=3)
+    }
+)
+
+SimpleHttpOperator(
+    task_id='tradeGPT_option_update_2',
+    method='GET',
+    http_conn_id='tradegpt',
+    endpoint="/option_update/run_option_update",
+    response_check=lambda response: response.json()['code'] == 200,
+    dag=tradeGPT_option_update_2,
+)
+
+tradeGPT_option_update_3 = DAG(
+    "tradeGPT_option_update_3",
+    catchup=False,
+    start_date=pendulum.datetime(2023, 7, 17, tz='Asia/Shanghai'),
+    schedule_interval="2-56/30 0-4 * * 1-5",
+    default_args={
+        "owner": "huangdexi",
+        "depends_on_past": False,
+        "retries": 1,
+        "retry_delay": timedelta(minutes=3)
+    }
+)
+
+SimpleHttpOperator(
+    task_id='tradeGPT_option_update_3',
+    method='GET',
+    http_conn_id='tradegpt',
+    endpoint="/option_update/run_option_update",
+    response_check=lambda response: response.json()['code'] == 200,
+    dag=tradeGPT_option_update_3,
+)
