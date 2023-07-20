0d9b68a25165fb13b065c9960661484f92173dc8
yuzhiqiang​ <yuzhiqiang@flowcapai.com>
2023-07-20 20:49:12 +0800

    clear_error_order_margin

--- dags/rockflow/dags/clearErrorOrderMargin.py
+++ dags/rockflow/dags/clearErrorOrderMargin.py
@@ -4,8 +4,8 @@ from datetime import timedelta
 from airflow.providers.http.operators.http import SimpleHttpOperator
 
 # 定时任务 - 12小时调用一次
-clearErrorOrderMargin = DAG(
-    "clearErrorOrderMargin",
+clear_error_order_margin = DAG(
+    "clear_error_order_margin",
     catchup=False,
     start_date=pendulum.datetime(2023, 7, 25, tz='Asia/Shanghai'),
     schedule_interval='0 */12 * * *',
@@ -18,11 +18,11 @@ clearErrorOrderMargin = DAG(
 )
 
 SimpleHttpOperator(
-    task_id='clearErrorOrderMargin',
+    task_id='clear_error_order_margin',
     method='PUT',
     http_conn_id='flow-feed-portfolio',
     endpoint='/account/inner/clear/brokerAccount/error/orderIds',
     response_check=lambda response: response.json()['code'] == 200,
     extra_options={"timeout": 60},
-    dag=clearErrorOrderMargin,
+    dag=clear_error_order_margin,
 )
