497c5c30a66574f5151a17bfef451d5784f7da83
maoboxuan <maoboxuan@flowcapai.com>
2023-07-21 10:31:39 +0800

    add order margin

---
+++ dags/rockflow/dags/clearErrorOrderMargin.py
@@ -0,0 +1,28 @@
+import pendulum
+from airflow.models import DAG
+from datetime import timedelta
+from airflow.providers.http.operators.http import SimpleHttpOperator
+
+# 定时任务 - 12小时调用一次
+clear_error_order_margin = DAG(
+    "clear_error_order_margin",
+    catchup=False,
+    start_date=pendulum.datetime(2023, 7, 25, tz='Asia/Shanghai'),
+    schedule_interval='0 */12 * * *',
+    default_args={
+        "owner": "yuzhiqiang",
+        "depends_on_past": False,
+        "retries": 3,
+        "retry_delay": timedelta(minutes=5),
+    }
+)
+
+SimpleHttpOperator(
+    task_id='clear_error_order_margin',
+    method='PUT',
+    http_conn_id='flow-feed-portfolio',
+    endpoint='/account/inner/clear/brokerAccount/error/orderIds',
+    response_check=lambda response: response.json()['code'] == 200,
+    extra_options={"timeout": 60},
+    dag=clear_error_order_margin,
+)
