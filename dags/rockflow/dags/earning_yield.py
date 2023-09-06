98869b9f7096251c432f59a4773fdda84afeefd1
maoboxuan <maoboxuan@flowcapai.com>
2023-09-06 20:02:32 +0800

    update

--- dags/rockflow/dags/earning_yield.py
+++ dags/rockflow/dags/earning_yield.py
@@ -51,3 +51,51 @@ SimpleHttpOperator(
     dag=earning_yield_leaderboard_update_10m,
 )
 
+# 收益率排行榜_1d
+earning_yield_rate_update_1d = DAG(
+    "earning_yield_rate_update_1d",
+    catchup=False,
+    # utc时间1点 对应美东夏令时20点或美东冬令时21点
+    start_date=pendulum.datetime(2022, 11, 4),
+    schedule_interval='0 1 * * *',
+    default_args={
+        "owner": "yuzhiqiang",
+        "depends_on_past": False,
+        "retries": 5,
+        "retry_delay": timedelta(minutes=5),
+    }
+)
+
+SimpleHttpOperator(
+    task_id='earning_yield_rate_update_1d',
+    method='PATCH',
+    http_conn_id='flow-social',
+    endpoint='/social/inner/earningYield/leaderboards/update/rank/1d',
+    response_check=lambda response: response.json()['code'] == 200,
+    extra_options={"timeout": 200},
+    dag=earning_yield_rate_update_1d,
+)
+
+# 收益率排行榜_10min
+day_earning_yield_rate_update_10m = DAG(
+    "day_earning_yield_rate_update_10m",
+    catchup=False,
+    start_date=pendulum.datetime(2022, 11, 4, tz='America/New_York'),
+    schedule_interval='*/10 * * * *',
+    default_args={
+        "owner": "yuzhiqiang",
+        "depends_on_past": False,
+        "retries": 0
+    }
+)
+
+SimpleHttpOperator(
+    task_id='day_earning_yield_rate_update_10m',
+    method='PATCH',
+    http_conn_id='flow-social',
+    endpoint='/social/inner/earningYield/leaderboards/update/rank/10m',
+    response_check=lambda response: response.json()['code'] == 200,
+    extra_options={"timeout": 200},
+    dag=day_earning_yield_rate_update_10m,
+)
+
