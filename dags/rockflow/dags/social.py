a071f7d26cf6300e009c67daa15535cc288f49a8
maoboxuan <maoboxuan@flowcapai.com>
2023-09-26 18:03:24 +0800

    add refresh earningYieldFilters

--- dags/rockflow/dags/social.py
+++ dags/rockflow/dags/social.py
@@ -71,3 +71,25 @@ SimpleHttpOperator(
     extra_options={"timeout": 60},
     dag=social_can_read_moments_refresh,
 )
+
+earning_yield_filter_refresh = DAG(
+    "earning_yield_filter_refresh",
+    catchup=False,
+    start_date=pendulum.datetime(2023, 9, 20, tz='America/New_York'),
+    schedule_interval='*/5 * * * *',
+    default_args={
+        "owner": "maoboxuan",
+        "depends_on_past": False,
+        "retries": 0,
+    }
+)
+
+SimpleHttpOperator(
+    task_id='earning_yield_filter_refresh',
+    method='POST',
+    http_conn_id='flow-social',
+    endpoint='/social/inner/earningYield/filters/cache',
+    response_check=lambda response: response.json()['code'] == 200,
+    extra_options={"timeout": 60},
+    dag=earning_yield_filter_refresh,
+)
