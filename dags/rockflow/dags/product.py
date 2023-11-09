301273f108e92b2da0521c65b9698142074d42dd
yuzhiqiang​ <yuzhiqiang@flowcapai.com>
2023-11-09 12:00:06 +0800

    update

--- dags/rockflow/dags/product.py
+++ dags/rockflow/dags/product.py
@@ -28,3 +28,51 @@ SimpleHttpOperator(
     extra_options={"timeout": 60},
     dag=place_orders,
 )
+
+GENERATE_PRODUCT_TASK = DAG(
+    "DAILY_DUSK_ALL_SENDING",
+    catchup=False,
+    start_date=pendulum.datetime(2023, 1, 14, tz='Asia/Shanghai'),
+    schedule_interval='00 12 * * 7',
+    default_args={
+        "owner": "chengwei",
+        "depends_on_past": False,
+        "retries": 5,
+        "retry_delay": timedelta(minutes=1),
+    }
+)
+
+SimpleHttpOperator(
+    task_id='ticks',
+    method='POST',
+    http_conn_id='flow-ticker-service',
+    endpoint='/ticker/inner/products/task',
+    response_check=lambda response: response.json()['code'] == 200,
+    extra_options={"timeout": 60},
+    dag=GENERATE_PRODUCT_TASK,
+)
+
+
+
+VIRTUAL_ORDER_TASK = DAG(
+    "VIRTUAL_ORDER_TASK",
+    catchup=False,
+    start_date=pendulum.datetime(2023, 1, 14, tz='Asia/Shanghai'),
+    schedule_interval='30 */2 * * *',
+    default_args={
+        "owner": "chengwei",
+        "depends_on_past": False,
+        "retries": 5,
+        "retry_delay": timedelta(minutes=1),
+    }
+)
+
+SimpleHttpOperator(
+    task_id='ticks',
+    method='POST',
+    http_conn_id='flow-portfolio-service',
+    endpoint='/portfolio/api/products/virtualOrder',
+    response_check=lambda response: response.json()['code'] == 200,
+    extra_options={"timeout": 60},
+    dag=VIRTUAL_ORDER_TASK,
+)
