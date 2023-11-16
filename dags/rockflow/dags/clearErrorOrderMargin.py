1a2b5936009b1bedad79933410c6521b5df3112a
yuzhiqiang​ <yuzhiqiang@flowcapai.com>
2023-11-16 14:35:19 +0800

    clear order margin every 1m

--- dags/rockflow/dags/clearErrorOrderMargin.py
+++ dags/rockflow/dags/clearErrorOrderMargin.py
@@ -26,3 +26,25 @@ SimpleHttpOperator(
     extra_options={"timeout": 60},
     dag=clear_error_order_margin,
 )
+
+clear_error_order_margin_1m = DAG(
+    "clear_error_order_margin_1m",
+    catchup=False,
+    start_date=pendulum.datetime(2023, 10, 14, tz='Asia/Shanghai'),
+    schedule_interval='*/1 * * * *',
+    default_args={
+        "owner": "yuzhiqiang",
+        "depends_on_past": False,
+        "retries": 0,
+    }
+)
+
+SimpleHttpOperator(
+    task_id='clear_error_order_margin_1m',
+    method='PUT',
+    http_conn_id='flow-feed-portfolio',
+    endpoint='/account/inner/clear/brokerAccount/error/orderIds/1m',
+    response_check=lambda response: response.json()['code'] == 200,
+    extra_options={"timeout": 60},
+    dag=clear_error_order_margin_1m,
+)
