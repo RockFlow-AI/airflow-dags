96a9bc169f5db6f0c5fca55e05c2b93435bd952a
yinxiang <yinxiang@flowcapai.com>
2023-11-28 17:43:09 +0800

    product task dependency and order patrol

--- dags/rockflow/dags/product.py
+++ dags/rockflow/dags/product.py
@@ -1,39 +1,16 @@
 import time
-from datetime import datetime, timedelta
+from datetime import timedelta
 
 import pendulum
 from airflow.models import DAG
 from airflow.providers.http.operators.http import SimpleHttpOperator
 
 # 理财产品
-place_orders = DAG(
-    "place_orders_fund",
-    catchup=False,
-    start_date=pendulum.datetime(2023, 10, 30, tz='Asia/Shanghai'),
-    schedule_interval='0 12 * * 0',
-    default_args={
-        "owner": "yinxiang",
-        "depends_on_past": False,
-        "retries": 5,
-    }
-)
-
-SimpleHttpOperator(
-    task_id='place_orders',
-    method='POST',
-    http_conn_id='flow-portfolio-service',
-    endpoint='/portfolio/inner/products/orders',
-    headers={'appId': '1'},
-    response_check=lambda response: response.json()['code'] == 200,
-    extra_options={"timeout": 60},
-    dag=place_orders,
-)
-
 GENERATE_PRODUCT_TASK = DAG(
     "GENERATE_PRODUCT_TASK",
     catchup=False,
     start_date=pendulum.datetime(2023, 1, 14, tz='Asia/Shanghai'),
-    schedule_interval='00 12 * * 7',
+    schedule_interval='30 11 * * 7',
     default_args={
         "owner": "chengwei",
         "depends_on_past": False,
@@ -42,7 +19,7 @@ GENERATE_PRODUCT_TASK = DAG(
     }
 )
 
-SimpleHttpOperator(
+create_product = SimpleHttpOperator(
     task_id='ticks',
     method='POST',
     http_conn_id='flow-ticker-service',
@@ -52,7 +29,19 @@ SimpleHttpOperator(
     dag=GENERATE_PRODUCT_TASK,
 )
 
+place_order = SimpleHttpOperator(
+    task_id='place_orders',
+    method='POST',
+    http_conn_id='flow-portfolio-service',
+    endpoint='/portfolio/inner/products/orders',
+    headers={'appId': '1'},
+    response_check=lambda response: response.json()['code'] == 200,
+    extra_options={"timeout": 60},
+    dag=GENERATE_PRODUCT_TASK,
+)
 
+create_product.post_execute = lambda **x: time.sleep(30)
+create_product >> place_order
 
 VIRTUAL_ORDER_TASK = DAG(
     "VIRTUAL_ORDER_TASK",
@@ -71,8 +60,31 @@ SimpleHttpOperator(
     task_id='ticks',
     method='POST',
     http_conn_id='flow-portfolio-service',
-    endpoint='/portfolio/api/products/virtualOrder',
+    endpoint='/portfolio/inner/products/virtualOrder',
     response_check=lambda response: response.json()['code'] == 200,
     extra_options={"timeout": 60},
     dag=VIRTUAL_ORDER_TASK,
+)
+
+PATROL_ORDER_TASK = DAG(
+    "PATROL_ORDER_TASK",
+    catchup=False,
+    start_date=pendulum.datetime(2023, 11, 28, tz='America/New_York'),
+    schedule_interval='0 10 * * 5',
+    default_args={
+        "owner": "yinxiang",
+        "depends_on_past": False,
+        "retries": 5,
+        "retry_delay": timedelta(minutes=1),
+    }
+)
+
+SimpleHttpOperator(
+    task_id='ticks',
+    method='PATCH',
+    http_conn_id='flow-portfolio-service',
+    endpoint='/portfolio/inner/products/patrol',
+    response_check=lambda response: response.json()['code'] == 200,
+    extra_options={"timeout": 60},
+    dag=PATROL_ORDER_TASK,
 )
