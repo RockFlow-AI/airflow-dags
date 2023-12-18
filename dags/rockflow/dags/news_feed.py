75ed6d54848121768ba0abb71ef59efcaf8f2ab3
yinxiang <yinxiang@flowcapai.com>
2023-12-18 11:33:38 +0800

    news feed virtual like

--- dags/rockflow/dags/news_feed.py
+++ dags/rockflow/dags/news_feed.py
@@ -24,3 +24,27 @@ SimpleHttpOperator(
     response_check=lambda response: response.json()['code'] == 200,
     dag=user_feed_clear,
 )
+
+
+news_feed_virtual_like_task = DAG(
+    "news_feed_virtual_like",
+    catchup=False,
+    start_date=pendulum.datetime(2023, 12, 19, tz='America/New_York'),
+    schedule_interval='*/10 * * * *',
+    default_args={
+        "owner": "yinxiang",
+        "depends_on_past": False,
+        "retries": 3,
+        "retry_delay": timedelta(minutes=1),
+    }
+)
+
+SimpleHttpOperator(
+    task_id='news_feed_virtual_like',
+    method='PATCH',
+    http_conn_id='flow-news',
+    endpoint='/news/inner/feeds',
+    response_check=lambda response: response.json()['code'] == 200,
+    extra_options={"timeout": 60},
+    dag=news_feed_virtual_like_task,
+)
