9b457995791d54d1de0b80b232efa4ccb820887d
calvertcao <calvertcao@outlook.com>
2023-12-29 18:14:07 +0800

    add rockbot config update module

--- dags/rockflow/dags/rockbot_update.py
+++ dags/rockflow/dags/rockbot_update.py
@@ -3,25 +3,22 @@ from airflow import DAG
 import pendulum
 from airflow.providers.http.operators.http import SimpleHttpOperator
 
-
-rockbot_update_10m = DAG(
-    "rockbot_update_10m",
+with DAG(
+    "rockbot_refresh_config",
     catchup=False,
     start_date=pendulum.datetime(2023, 9, 1),
-    schedule_interval='*/30 * * * *',
+    schedule_interval='0 4 * * *',
     default_args={
         "owner": "caohaoxuan",
         "depends_on_past": False,
         "retries": 0,
     }
-)
-
-SimpleHttpOperator(
-    task_id='rockbot_update_10m',
-    method='POST',
-    http_conn_id='rockbot',
-    endpoint='/bot/api/ideas/update_10m',
-    response_check=lambda response: response.json()['code'] == 200,
-    extra_options={"timeout": 60},
-    dag=rockbot_update_10m,
-)
+) as rockbot_refresh_config:
+    # refresh config
+    rockbot_refresh_config = SimpleHttpOperator(
+        task_id='rockbot_refresh_config',
+        method='POST',
+        http_conn_id='rockbot',
+        endpoint='/bot/api/config/refresh_config',
+        response_check=lambda response: response.json()['code'] == 200,
+    )
