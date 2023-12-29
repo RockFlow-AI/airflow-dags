b5668180b83b1b0f5204c4bf34be0c0eca99988b
calvertcao <calvertcao@outlook.com>
2023-12-29 16:32:05 +0800

    add news_feed_python module

---
+++ dags/rockflow/dags/news_feed_python.py
@@ -0,0 +1,97 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+import pendulum
+from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.models.baseoperator import chain
+# scrap DAG for Monday to Friday
+with DAG(
+    "feed_news_scraping_weekdays",
+    catchup=False,
+    start_date=pendulum.datetime(2023, 9, 1),
+    schedule_interval='0 10,13,16,18,21 * * 1-5',  # Cron expression for specific times on Monday to Friday
+    default_args={
+        "owner": "caohaoxuan",
+        "depends_on_past": False,
+        "retries": 0,
+    }
+) as feed_news_scraping_weekdays:
+    # scrap task for weekdays
+    scraping_task_weekdays = SimpleHttpOperator(
+        task_id='feed_news_scraping_weekdays',
+        method='POST',
+        http_conn_id='rockbot',
+        endpoint='/bot/api/ideas/feed/news/scrap',
+        response_check=lambda response: response.json()['code'] == 200,
+        dag=feed_news_scraping_weekdays
+    )
+# scrap DAG for Saturday and Sunday
+with DAG(
+    "feed_news_scraping_weekends",
+    catchup=False,
+    start_date=pendulum.datetime(2023, 9, 1),
+    schedule_interval='0 15 * * 6,0',  # Cron expression for 3 PM on Saturday and Sunday
+    default_args={
+        "owner": "caohaoxuan",
+        "depends_on_past": False,
+        "retries": 0,
+    }
+) as feed_news_scraping_weekends:
+    # Task for weekends
+    scraping_task_weekends = SimpleHttpOperator(
+        task_id='feed_news_scraping_weekends',
+        method='POST',
+        http_conn_id='rockbot',
+        endpoint='/bot/api/ideas/feed/news/scrap',
+        response_check=lambda response: response.json()['code'] == 200,
+        dag=feed_news_scraping_weekends
+    )
+# test send DAG for Monday to Friday
+with DAG(
+    "feed_news_inspiration_message_test",
+    catchup=False,
+    start_date=pendulum.datetime(2023, 9, 1),
+    schedule_interval='*/10 * * * *',  # Cron expression for specific times on Monday to Friday
+    default_args={
+        "owner": "caohaoxuan",
+        "depends_on_past": False,
+        "retries": 0,
+    }
+) as feed_news_inspiration_message_test:
+    task_feed_news_inspiration_message_test = SimpleHttpOperator(
+        task_id='feed_news_inspiration_message_test',
+        method='POST',
+        http_conn_id='rockbot',
+        endpoint='/bot/api/ideas/feed/news/test_send',
+        response_check=lambda response: response.json()['code'] == 200,
+    )
+#  DAG for every day
+with DAG(
+    "feed_news_analyze_and_generate",
+    catchup=False,
+    start_date=pendulum.datetime(2023, 9, 1),
+    schedule_interval='*/10 * * * *',  # Cron expression for specific times on Monday to Friday
+    default_args={
+        "owner": "caohaoxuan",
+        "depends_on_past": False,
+        "retries": 0,
+    }
+) as feed_news_analyze_and_generate:
+    # Task for analyze
+    analyze_task = SimpleHttpOperator(
+        task_id='feed_news_analyze',
+        method='POST',
+        http_conn_id='rockbot',
+        endpoint='/bot/api/ideas/feed/news/analyze',
+        response_check=lambda response: response.json()['code'] == 200,
+    )
+    # Task for generate
+    generate_task = SimpleHttpOperator(
+        task_id='feed_news_generate',
+        method='POST',
+        http_conn_id='rockbot',
+        endpoint='/bot/api/ideas/feed/news/generate',
+        response_check=lambda response: response.json()['code'] == 200,
+    )
+    analyze_task >> generate_task
+
+
