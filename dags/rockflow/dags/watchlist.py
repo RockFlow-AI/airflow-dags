357dc73f7e673595216a32c84707863b683baab7
sunfulin <sunfulin@flowcapai.com>
2024-07-23 11:00:54 +0800

    flush watchlist

--- dags/rockflow/dags/watchlist.py
+++ dags/rockflow/dags/watchlist.py
@@ -98,3 +98,26 @@ SimpleHttpOperator(
     extra_options={"timeout": 60},
     dag=RELOAD_HOT_WATCHLIST,
 )
+
+FLUSH_HOT_WATCHLIST = DAG(
+    "FLUSH_HOT_WATCHLIST",
+    catchup=False,
+    start_date=pendulum.datetime(2024, 7, 23, tz='Asia/Shanghai'),
+    schedule_interval='0 0 6 * *',
+    default_args={
+        "owner": "sunfulin",
+        "depends_on_past": False,
+        "retries": 3,
+        "retry_delay": timedelta(minutes=5),
+    }
+)
+
+SimpleHttpOperator(
+    task_id='FLUSH_HOT_WATCHLIST',
+    method='PUT',
+    http_conn_id='flow-watchlist',
+    endpoint='/inner/watchlist/popular/cache',
+    response_check=lambda response: response.json()['code'] == 200,
+    extra_options={"timeout": 60},
+    dag=FLUSH_HOT_WATCHLIST,
+)
