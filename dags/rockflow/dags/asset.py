354d08875691dd6f1fb2405cbec71730ea7624ee
chengwei <2281795275@qq.com>
2024-07-30 15:31:16 +0800

    asset stat
    (cherry picked from commit 18a66ba99d05dd5bcfdd4da206c04e96722e9bab)

--- dags/rockflow/dags/asset.py
+++ dags/rockflow/dags/asset.py
@@ -26,3 +26,49 @@ SimpleHttpOperator(
     extra_options={"timeout": 60},
     dag=asset,
 )
+
+# 刷新存钱罐资产
+refresh_money_box = DAG(
+    "refresh_money_box",
+    catchup=False,
+    start_date=pendulum.datetime(2023, 7, 12, tz='Asia/Shanghai'),
+    schedule_interval='0 */6 * * *',
+    default_args={
+        "owner": "chengwei",
+        "depends_on_past": False,
+        "retries": 0,
+    }
+)
+
+SimpleHttpOperator(
+    task_id='refresh_money_box',
+    method='POST',
+    http_conn_id='flow-ledger',
+    endpoint='/ledger/inner/money-box/refreshMoneyBox?date={date}'.format(date=datetime.now().strftime("%Y-%m-%d")),
+    response_check=lambda response: response.json()['code'] == 200,
+    extra_options={"timeout": 600},
+    dag=refresh_money_box,
+)
+
+
+asset_stat = DAG(
+    "asset_stat",
+    catchup=False,
+    start_date=pendulum.datetime(2023, 7, 12, tz='America/New_York'),
+    schedule_interval='30 8 * * *',
+    default_args={
+        "owner": "chengwei",
+        "depends_on_past": False,
+        "retries": 0,
+    }
+)
+
+SimpleHttpOperator(
+    task_id='asset_stat',
+    method='PUT',
+    http_conn_id='flow-ledger',
+    endpoint='ledger/inner/accountAsset/stat/markets/US/{date}'.format(date=datetime.now().strftime("%Y-%m-%d")),
+    response_check=lambda response: response.json()['code'] == 200,
+    extra_options={"timeout": 600},
+    dag=asset_stat,
+)
