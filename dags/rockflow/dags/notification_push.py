1f7445cf022fdbc0567631ad7b4bb696cc923f8a
sunfulin <sunfulin@flowcapai.com>
2024-09-05 15:51:00 +0800

    update

--- dags/rockflow/dags/notification_push.py
+++ dags/rockflow/dags/notification_push.py
@@ -309,12 +309,12 @@ SimpleHttpOperator(
     dag=PUSH_TO_LIMIT_ORDER_UNSETTLED_US_3,
 )
 
-
+# 这里的时间 记得改
 PUSH_TO_LIMIT_ORDER_UNSETTLED_US_4 = DAG(
     "PUSH_TO_LIMIT_ORDER_UNSETTLED_US_4",
     catchup=False,
     start_date=pendulum.datetime(2024, 8, 28, tz='Asia/Shanghai'),
-    schedule_interval='*/1 11-16 * * 1-5',
+    schedule_interval='*/1 11-20 * * 1-5',
     default_args={
         "owner": "sunfulin",
         "depends_on_past": False,
