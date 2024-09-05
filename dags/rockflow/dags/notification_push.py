b73ee3981134e63bcd510dfe5d594d8c0bf620c8
sunfulin <sunfulin@flowcapai.com>
2024-09-05 10:34:03 +0800

    update

--- dags/rockflow/dags/notification_push.py
+++ dags/rockflow/dags/notification_push.py
@@ -142,11 +142,11 @@ SimpleHttpOperator(
 
 
 # 定时任务 每分钟检测
-PUSH_TO_LIMIT_ORDER_UNSETTLED = DAG(
+PUSH_TO_LIMIT_ORDER_UNSETTLED_1 = DAG(
     "PUSH_TO_LIMIT_ORDER_UNSETTLED",
     catchup=False,
     start_date=pendulum.datetime(2024, 8, 28, tz='Asia/Shanghai'),
-    schedule_interval='* * * * *',
+    schedule_interval='45-59 9 * * 1-5',
     default_args={
         "owner": "sunfulin",
         "depends_on_past": False,
@@ -155,63 +155,38 @@ PUSH_TO_LIMIT_ORDER_UNSETTLED = DAG(
 )
 
 SimpleHttpOperator(
-    task_id='PUSH_TO_LIMIT_ORDER_UNSETTLED',
+    task_id='PUSH_TO_LIMIT_ORDER_UNSETTLED_1',
     method='PATCH',
     http_conn_id='flow-notification',
-    endpoint='/portfolio/inner/pendingOrders?interval=15&session=0&source=HK',
+    endpoint='/portfolio/inner/pendingOrders?interval=15&session=0&source=HK,OSHK',
     response_check=lambda response: response.json()['code'] == 200,
     extra_options={"timeout": 200},
-    dag=PUSH_TO_LIMIT_ORDER_UNSETTLED,
+    dag=PUSH_TO_LIMIT_ORDER_UNSETTLED_1,
 )
-#
-# SimpleHttpOperator(
-#     task_id='push_to_limit_order_unsettled',
-#     method='PATCH',
-#     http_conn_id='flow-notification',
-#     endpoint='/portfolio/inner/pendingOrders?interval=60&session=0&source=HK',
-#     response_check=lambda response: response.json()['code'] == 200,
-#     extra_options={"timeout": 200},
-#     dag=push_to_unregister,
-# )
-#
-# SimpleHttpOperator(
-#     task_id='push_to_limit_order_unsettled',
-#     method='PATCH',
-#     http_conn_id='flow-notification',
-#     endpoint='/portfolio/inner/pendingOrders?interval=15&session=0&source=US',
-#     response_check=lambda response: response.json()['code'] == 200,
-#     extra_options={"timeout": 200},
-#     dag=push_to_unregister,
-# )
-#
-# SimpleHttpOperator(
-#     task_id='push_to_limit_order_unsettled',
-#     method='PATCH',
-#     http_conn_id='flow-notification',
-#     endpoint='/portfolio/inner/pendingOrders?interval=15&session=1&source=US',
-#     response_check=lambda response: response.json()['code'] == 200,
-#     extra_options={"timeout": 200},
-#     dag=push_to_unregister,
-# )
-#
-# SimpleHttpOperator(
-#     task_id='push_to_limit_order_unsettled',
-#     method='PATCH',
-#     http_conn_id='flow-notification',
-#     endpoint='/portfolio/inner/pendingOrders?interval=60&session=0&source=US',
-#     response_check=lambda response: response.json()['code'] == 200,
-#     extra_options={"timeout": 200},
-#     dag=push_to_unregister,
-# )
-#
-# SimpleHttpOperator(
-#     task_id='push_to_limit_order_unsettled',
-#     method='PATCH',
-#     http_conn_id='flow-notification',
-#     endpoint='/portfolio/inner/pendingOrders?interval=60&session=1&source=US',
-#     response_check=lambda response: response.json()['code'] == 200,
-#     extra_options={"timeout": 200},
-#     dag=push_to_unregister,
-# )
+
+PUSH_TO_LIMIT_ORDER_UNSETTLED_2 = DAG(
+    "PUSH_TO_LIMIT_ORDER_UNSETTLED_2",
+    catchup=False,
+    start_date=pendulum.datetime(2024, 8, 28, tz='Asia/Shanghai'),
+    schedule_interval='*/1 10-17 * * 1-5',
+    default_args={
+        "owner": "sunfulin",
+        "depends_on_past": False,
+        "retries": 0
+    }
+)
+
+SimpleHttpOperator(
+    task_id='PUSH_TO_LIMIT_ORDER_UNSETTLED_2',
+    method='PATCH',
+    http_conn_id='flow-notification',
+    endpoint='/portfolio/inner/pendingOrders?interval=15&session=0&source=HK,OSHK',
+    response_check=lambda response: response.json()['code'] == 200,
+    extra_options={"timeout": 200},
+    dag=PUSH_TO_LIMIT_ORDER_UNSETTLED_2,
+)
+
+
+
 
 
