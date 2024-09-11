6af2e367d10cd4ea6cb11052f6cd21b15d171201
liushihao <liushihao@flowcapai.com>
2024-09-11 18:24:36 +0800

    add rockbot clean_expired_graphs task

--- dags/rockflow/dags/rockbot_update.py
+++ dags/rockflow/dags/rockbot_update.py
@@ -22,3 +22,23 @@ with DAG(
         endpoint='/bot/api/config/refresh_config',
         response_check=lambda response: response.json()['code'] == 200,
     )
+    
+with DAG(
+    "rockbot_clean_expired_graphs",
+    catchup=False,
+    start_date=pendulum.datetime(2024, 9, 10),
+    schedule_interval="*/60 * * * *",
+    default_args={
+        "owner": "caohaoxuan",
+        "depends_on_past": False,
+        "retries": 0,
+    },
+) as rockbot_clean_expired_graphs:
+    # rockbot_clean_expired_graphs
+    rockbot_clean_expired_graphs_task = SimpleHttpOperator(
+        task_id="rockbot_clean_expired_graphs",
+        method="POST",
+        http_conn_id="rockbot",
+        endpoint="/bot/api/graphs/clean_expired_graphs",
+        response_check=lambda response: response.json()["code"] == 200,
+    )
