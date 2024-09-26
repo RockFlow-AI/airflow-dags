201391a2f5a686c9dedbf416caf5e51f69667560
yuzhiqiang​ <yuzhiqiang@flowcapai.com>
2024-09-26 17:57:19 +0800

    telegram message

--- dags/rockflow/dags/notification_strategy.py
+++ dags/rockflow/dags/notification_strategy.py
@@ -25,3 +25,25 @@ SimpleHttpOperator(
     extra_options={"timeout": 60},
     dag=send_msg_by_config_1m,
 )
+
+send_telegram_msg_1m = DAG(
+    "send_telegram_msg_1m",
+    catchup=False,
+    start_date=pendulum.datetime(2024, 8, 25, tz='America/New_York'),
+    schedule_interval='*/1 * * * *',
+    default_args={
+        "owner": "yuzhiqiang",
+        "depends_on_past": False,
+        "retries": 0
+    }
+)
+
+SimpleHttpOperator(
+    task_id='send_telegram_msg_1m',
+    method='PUT',
+    http_conn_id='flow-notification-strategy',
+    endpoint='/notification/telegram/inner/send/scheduled/messages',
+    response_check=lambda response: response.json()['code'] == 200,
+    extra_options={"timeout": 60},
+    dag=send_telegram_msg_1m,
+)
