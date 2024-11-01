3cbd6fb8939e05ab29a5c3e0c399e48c71c777e0
yuzhiqiang​ <yuzhiqiang@flowcapai.com>
2024-11-01 10:52:56 +0800

    add user_group_send_message_1d

--- dags/rockflow/dags/message_bot.py
+++ dags/rockflow/dags/message_bot.py
@@ -27,3 +27,25 @@ SimpleHttpOperator(
     extra_options={"timeout": 60},
     dag=message_bot,
 )
+
+user_group_send_message_1d = DAG(
+    "user_group_send_message_1d",
+    catchup=False,
+    start_date=pendulum.datetime(2024, 11, 1, tz='Asia/Shanghai'),
+    schedule_interval='0 18 * * *',
+    default_args={
+        "owner": "yuzhiqiang",
+        "depends_on_past": False,
+        "retries": 0
+    }
+)
+
+SimpleHttpOperator(
+    task_id='user_group_send_message_1d',
+    method='PUT',
+    http_conn_id='flow-admin',
+    endpoint='/admin/user/group/send/1d',
+    response_check=lambda response: response.json()['code'] == 200,
+    extra_options={"timeout": 60},
+    dag=user_group_send_message_1d,
+)
