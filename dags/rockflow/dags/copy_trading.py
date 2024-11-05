a2ab2162f9577fbec973429e861538c518881ea5
chengwei <2281795275@qq.com>
2024-11-05 10:00:44 +0800

    copy trading

--- dags/rockflow/dags/copy_trading.py
+++ dags/rockflow/dags/copy_trading.py
@@ -1,48 +1,50 @@
 import pendulum
 from airflow.models import DAG
+from datetime import datetime, timedelta
 from airflow.providers.http.operators.http import SimpleHttpOperator
 
-# 盘前5分钟跟单任务扫描
-copy_trading_init_position = DAG(
-    "copy_trading_init_position",
+copy_trading_copy_position = DAG(
+    "copy_trading_copy_position",
     catchup=False,
-    start_date=pendulum.datetime(2022, 11, 4, tz='America/New_York'),
+    start_date=pendulum.datetime(2024, 10, 14, tz='America/New_York'),
     schedule_interval='35 9 * * *',
     default_args={
         "owner": "chengwei",
         "depends_on_past": False,
-        "retries": 0,
+        "retries": 5,
+        "retry_delay": timedelta(minutes=1),
     }
 )
 
 SimpleHttpOperator(
-    task_id='copy_trading_init_position',
-    method='GET',
+    task_id='copy_trading_copy_position',
+    method='PUT',
     http_conn_id='flow-social',
     endpoint='/social/inner/copyTrading/position/init',
     response_check=lambda response: response.json()['code'] == 200,
-    extra_options={"timeout": 3600},
-    dag=copy_trading_init_position,
+    extra_options={"timeout": 60},
+    dag=copy_trading_copy_position,
 )
 
-copy_trading_correct_position = DAG(
-    "copy_trading_correct_position",
+copy_trading_auto_correction = DAG(
+    "copy_trading_auto_correction",
     catchup=False,
-    start_date=pendulum.datetime(2022, 11, 4, tz='America/New_York'),
+    start_date=pendulum.datetime(2024, 10, 14, tz='America/New_York'),
     schedule_interval='40 9 * * *',
     default_args={
         "owner": "chengwei",
         "depends_on_past": False,
-        "retries": 0,
+        "retries": 5,
+        "retry_delay": timedelta(minutes=1),
     }
 )
 
 SimpleHttpOperator(
-    task_id='copy_trading_correct_position',
-    method='GET',
+    task_id='copy_trading_auto_correction',
+    method='PUT',
     http_conn_id='flow-social',
-    endpoint='/social/inner/copyTrading/position/correct',
+    endpoint='/social/inner/copyTrading/position/correct?tradeDay={date}'.format(date=datetime.now().strftime("%Y-%m-%d")),
     response_check=lambda response: response.json()['code'] == 200,
-    extra_options={"timeout": 3600},
-    dag=copy_trading_correct_position,
+    extra_options={"timeout": 60},
+    dag=copy_trading_auto_correction,
 )
