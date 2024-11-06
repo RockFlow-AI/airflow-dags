db52e8440029b02c37dad452fd49dcc37bb89125
yuzhiqiang​ <yuzhiqiang@flowcapai.com>
2024-11-06 18:55:43 +0800

    update

--- dags/rockflow/dags/watchlist.py
+++ dags/rockflow/dags/watchlist.py
@@ -72,3 +72,26 @@ SimpleHttpOperator(
     extra_options={"timeout": 3600},
     dag=FLUSH_HOT_WATCHLIST,
 )
+
+
+FLUSH_HOT_WATCHLIST1 = DAG(
+    "FLUSH_HOT_WATCHLIST1",
+    catchup=False,
+    start_date=pendulum.datetime(2024, 7, 23, tz='Asia/Shanghai'),
+    schedule_interval='00 18 * * *',
+    default_args={
+        "owner": "sunfulin",
+        "depends_on_past": False,
+        "retries": 0,
+    }
+)
+
+SimpleHttpOperator(
+    task_id='FLUSH_HOT_WATCHLIST1',
+    method='GET',
+    http_conn_id='flow-watchlist',
+    endpoint='/inner/watchlist/popular/cache',
+    response_check=lambda response: response.json()['code'] == 200,
+    extra_options={"timeout": 3600},
+    dag=FLUSH_HOT_WATCHLIST1,
+)
