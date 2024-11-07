7e5a36c1821382493d6d4fb1404c0847132fa375
chengwei <2281795275@qq.com>
2024-11-07 20:04:45 +0800

    account_asset_sync

---
+++ dags/rockflow/dags/asset_account.py
@@ -0,0 +1,47 @@
+import pendulum
+from airflow.models import DAG
+from airflow.providers.http.operators.http import SimpleHttpOperator
+
+account_asset_sync_us = DAG(
+    "account_asset_sync_us",
+    catchup=False,
+    start_date=pendulum.datetime(2024, 11, 4, tz='America/New_York'),
+    schedule_interval='0 0 * * *',
+    default_args={
+        "owner": "chengwei",
+        "depends_on_past": False,
+        "retries": 0,
+    }
+)
+
+SimpleHttpOperator(
+    task_id='account_asset_sync_us',
+    method='PUT',
+    http_conn_id='flow-ledger',
+    endpoint='/ledger/inner/accounts/sync/accountAssets',
+    response_check=lambda response: response.json()['code'] == 200,
+    extra_options={"timeout": 60},
+    dag=account_asset_sync_us,
+)
+
+account_asset_sync_hk = DAG(
+    "account_asset_sync_hk",
+    catchup=False,
+    start_date=pendulum.datetime(2024, 11, 4, tz='Asia/Shanghai'),
+    schedule_interval='0 0 * * *',
+    default_args={
+        "owner": "chengwei",
+        "depends_on_past": False,
+        "retries": 0,
+    }
+)
+
+SimpleHttpOperator(
+    task_id='account_asset_sync_hk',
+    method='PUT',
+    http_conn_id='flow-ledger',
+    endpoint='/ledger/inner/accounts/sync/accountAssets',
+    response_check=lambda response: response.json()['code'] == 200,
+    extra_options={"timeout": 60},
+    dag=account_asset_sync_hk,
+)
