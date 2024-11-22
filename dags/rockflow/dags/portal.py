9299806da5c6de9b480a2c40d7774882514d7364
yuzhiqiang​ <yuzhiqiang@flowcapai.com>
2024-11-22 11:29:08 +0800

    WATCHLIST_POOL_RELATIONS

--- dags/rockflow/dags/portal.py
+++ dags/rockflow/dags/portal.py
@@ -27,3 +27,26 @@ SimpleHttpOperator(
     extra_options={"timeout": 60},
     dag=refresh_etf_symbol,
 )
+
+RELOAD_HOT_WATCHLIST_POOL_RELATIONS = DAG(
+    "RELOAD_HOT_WATCHLIST_POOL_RELATIONS",
+    catchup=False,
+    start_date=pendulum.datetime(2024, 11, 20, tz='Asia/Shanghai'),
+    schedule_interval='00 19 * * *',
+    default_args={
+        "owner": "yuzhiqaing",
+        "depends_on_past": False,
+        "retries": 3,
+        "retry_delay": timedelta(minutes=5),
+    }
+)
+
+SimpleHttpOperator(
+    task_id='RELOAD_HOT_WATCHLIST_POOL_RELATIONS',
+    method='GET',
+    http_conn_id='flow-portal',
+    endpoint='/inner/portal/watchlist/pool/relations',
+    response_check=lambda response: response.json()['code'] == 200,
+    extra_options={"timeout": 3600},
+    dag=RELOAD_HOT_WATCHLIST_POOL_RELATIONS,
+)
