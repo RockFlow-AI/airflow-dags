52aa28248aca2d93fe5ae26b6c76616c695e4535
chengwei <2281795275@qq.com>
2024-12-16 14:34:09 +0800

    fix option_exercise_file

--- dags/rockflow/dags/file_feed-simulation.py
+++ dags/rockflow/dags/file_feed-simulation.py
@@ -4,8 +4,8 @@ from airflow.providers.http.operators.http import SimpleHttpOperator
 from datetime import datetime, timedelta
 
 
-option_exercise_file = DAG(
-    "option_exercise_file",
+option_exercise_file_simulation = DAG(
+    "option_exercise_file_simulation",
     catchup=False,
     start_date=pendulum.datetime(2024, 10, 9, tz='Asia/Shanghai'),
     schedule_interval='0 10 * * 1-7',
@@ -18,11 +18,11 @@ option_exercise_file = DAG(
 )
 
 SimpleHttpOperator(
-    task_id='option_exercise_file',
+    task_id='option_exercise_file_simulation',
     method='PUT',
     http_conn_id='flow-file-feed-simulation',
     endpoint='/file/inner/optionExercise/task?tradeDay={date}&market=US'.format(date=(datetime.now() + timedelta(days=-1)).strftime("%Y-%m-%d")),
     response_check=lambda response: response.json()['code'] == 200,
     extra_options={"timeout": 60 * 5},
-    dag=option_exercise_file,
+    dag=option_exercise_file_simulation,
 )
