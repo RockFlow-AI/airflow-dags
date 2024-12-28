7f9d1b8579a9f2e851e9ea9114c6e2eec84804c4
caohaoxuan <caohaoxuan@flowcapai.com>
2024-12-28 20:13:40 +0800

    feat: daily trading report dag update

--- dags/rockflow/dags/daily_trading_report.py
+++ dags/rockflow/dags/daily_trading_report.py
@@ -19,6 +19,25 @@ with DAG(
         task_id="daily_trading_report",
         method="POST",
         http_conn_id="rockbot",
-        endpoint="/bot/api/data/get/daily_trading_report",
+        endpoint="/bot/api/data/daily-trading-report",
+        response_check=lambda response: response.json()["code"] == 200,
+    )
+
+with DAG(
+    "this_month_data_report",
+    catchup=False,
+    start_date=pendulum.datetime(2024, 12, 4, tz="Asia/Shanghai"),
+    schedule_interval="0 9 * * 2-6",
+    default_args={
+        "owner": "caohaoxuan",
+        "depends_on_past": False,
+        "retries": 0,
+    },
+) as daily_trading_report:
+    daily_trading_report_task = SimpleHttpOperator(
+        task_id="this_month_data_report",
+        method="POST",
+        http_conn_id="rockbot",
+        endpoint="/bot/api/data/this-month-data-report",
         response_check=lambda response: response.json()["code"] == 200,
     )
