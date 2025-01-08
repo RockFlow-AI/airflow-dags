6183582d2b45fae7aa865f4f3ea0d28e29979fe0
yuzhiqiang​ <yuzhiqiang@flowcapai.com>
2025-01-08 11:26:56 +0800

    add clear_logo_file_content

---
+++ dags/rockflow/operators/clear_file_content.py
@@ -0,0 +1,25 @@
+from multiprocessing.pool import ThreadPool as Pool
+from typing import Any, Hashable
+
+import oss2
+from rockflow.operators.oss import OSSOperator
+
+
+class ClearFileContentOperator(OSSOperator):
+    def __init__(self,
+                 from_key: str,
+                 avatar_bucket_name: str,
+                 **kwargs) -> None:
+        super().__init__(**kwargs)
+        self.from_key = from_key
+        self.avatar_bucket_name = avatar_bucket_name
+
+    @property
+    def avatar_bucket(self) -> oss2.api.Bucket:
+        return self.oss_hook.get_bucket(self.avatar_bucket_name)
+
+    def clear_from_key_file(self):
+        self.clear_object(self.from_key)
+
+    def execute(self, context: Any):
+        self.clear_from_key_file()
