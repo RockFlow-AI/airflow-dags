8025f32d2d32cce0f706d749c06ecd2931375ac7
yuzhiqiang​ <yuzhiqiang@flowcapai.com>
2025-01-08 15:45:23 +0800

    add clear_logo_file_content

--- dags/rockflow/operators/clear_file_content.py
+++ dags/rockflow/operators/clear_file_content.py
@@ -1,8 +1,7 @@
-from multiprocessing.pool import ThreadPool as Pool
 from typing import Any, Hashable
-
 import oss2
 from rockflow.operators.oss import OSSOperator
+from airflow.exceptions import AirflowException
 
 
 class ClearFileContentOperator(OSSOperator):
@@ -19,9 +18,20 @@ class ClearFileContentOperator(OSSOperator):
         return self.oss_hook.get_bucket(self.avatar_bucket_name)
 
     def clear_from_key_file(self):
-        print(f"Clearing file content for key: {self.from_key}")
-        # 修复调用 put_object 的代码
+        print(f"Clearing file content for key: {self.from_key} in bucket: {self.avatar_bucket_name}")
         self.put_object(self.from_key, "")
 
     def execute(self, context):
         self.clear_from_key_file()
+
+    @staticmethod
+    def put_object_(bucket: oss2.api.Bucket, key: str, content):
+        try:
+            print(f"put_object: {key}")
+            result = bucket.put_object(key, content)  # 上传文件内容
+            print(f"Put object result: {result}")
+        except Exception as e:
+            raise AirflowException(f"Errors: {e}")
+
+    def put_object(self, key: str, content):
+        self.put_object_(self.avatar_bucket, key, content)
