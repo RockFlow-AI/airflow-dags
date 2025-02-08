49b378bcc02bdf1bda437f2cd4ec61fc8eb4ecb3
chengwei <2281795275@qq.com>
2025-02-08 12:10:29 +0800

    accountAssets sync

--- dags/rockflow/dags/asset_account.py
+++ dags/rockflow/dags/asset_account.py
@@ -1,5 +1,6 @@
 import pendulum
 from airflow.models import DAG
+from datetime import timedelta, datetime
 from airflow.providers.http.operators.http import SimpleHttpOperator
 
 account_asset_sync_us = DAG(
@@ -18,7 +19,7 @@ SimpleHttpOperator(
     task_id='account_asset_sync_us',
     method='PUT',
     http_conn_id='flow-ledger',
-    endpoint='/ledger/inner/accounts/sync/accountAssets',
+    endpoint='/ledger/inner/accounts/sync/accountAssets/activeUsers?startTime={date}'.format(date=(datetime.now() + timedelta(days=-10)).strftime("%Y-%m-%d")),
     response_check=lambda response: response.json()['code'] == 200,
     extra_options={"timeout": 60},
     dag=account_asset_sync_us,
@@ -40,7 +41,7 @@ SimpleHttpOperator(
     task_id='account_asset_sync_hk',
     method='PUT',
     http_conn_id='flow-ledger',
-    endpoint='/ledger/inner/accounts/sync/accountAssets',
+    endpoint='/ledger/inner/accounts/sync/accountAssets/activeUsers?startTime={date}'.format(date=(datetime.now() + timedelta(days=-10)).strftime("%Y-%m-%d")),
     response_check=lambda response: response.json()['code'] == 200,
     extra_options={"timeout": 60},
     dag=account_asset_sync_hk,
