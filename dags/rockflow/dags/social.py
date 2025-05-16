011d31d95d2d811411f7383df429f7cbbf3c8a56
yuzhiqiang​ <yuzhiqiang@flowcapai.com>
2025-05-16 11:50:24 +0800

    add refresh_copyTrading_profit

--- dags/rockflow/dags/social.py
+++ dags/rockflow/dags/social.py
@@ -139,3 +139,23 @@ SimpleHttpOperator(
     dag=copy_watchlist_task,
 )
 
+copy_watchlistrefresh_copyTrading_profit_task = DAG(
+    "refresh_copyTrading_profit",
+    catchup=False,
+    start_date=pendulum.datetime(2025, 5, 15, tz='America/New_York'),
+    schedule_interval='*/10 * * * *',
+    default_args={
+        "owner": "yuzhiqiang",
+        "depends_on_past": False,
+    }
+)
+
+SimpleHttpOperator(
+    task_id='refresh_copyTrading_profit',
+    method='PATCH',
+    http_conn_id='flow-social',
+    endpoint='/social/inner/copyTrading/refresh/profit',
+    response_check=lambda response: response.json()['code'] == 200,
+    extra_options={"timeout": 60},
+    dag=refresh_copyTrading_profit,
+)
