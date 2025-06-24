2d90774d4d3464c51c272d60bdf78c9f384b1db9
maoshanghui <maoshanghui@flowcapai.com>
2025-06-24 10:03:03 +0800

    fetch polygon time

--- dags/rockflow/dags/tick.py
+++ dags/rockflow/dags/tick.py
@@ -398,7 +398,7 @@ daily_last_tick_us_yahoo = DAG(
     "daily_last_tick_us_yahoo",
     catchup=False,
     start_date=pendulum.datetime(2023, 1, 14, tz='America/New_York'),
-    schedule_interval='50 16,17,19 * * 1-5',
+    schedule_interval='50 16,17 * * 1-5',
     default_args={
         "owner": "jingjiadong",
         "depends_on_past": False,
