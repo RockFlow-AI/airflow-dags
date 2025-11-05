b0cd0b070b792f861d27fbc51691b5c2e36e4df6
treepudding <58771250+treepudding@users.noreply.github.com>
2025-11-05 16:34:14 +0800

    Add files via upload

---
+++ dags/rockflow/dags/ai_arena_v2.py
@@ -0,0 +1,26 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+import pendulum
+from airflow.providers.http.operators.http import SimpleHttpOperator
+
+with DAG(
+    "rockbot_arena_trade_v2",
+    catchup=False,
+    start_date=pendulum.datetime(2025, 10, 23),
+    schedule_interval='*/5 * * * *',
+    default_args={
+        "owner": "caohaoxuan",
+        "depends_on_past": False,
+        "retries": 0,
+    }
+) as rockbot_arena_trade_v2:
+    # arena trade v2
+    rockbot_arena_trade_v2_task = SimpleHttpOperator(
+        task_id='rockbot_arena_trade_v2',
+        method='GET',
+        http_conn_id='rockbot',
+        endpoint='/bot/api/arena/v2/ai_arena_v2',
+        response_check=lambda response: response.json()['code'] == 200,
+    )
+
+
