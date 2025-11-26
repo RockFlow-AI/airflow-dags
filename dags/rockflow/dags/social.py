1fe38a8524703de5ca11258898286e48f9853476
maoshanghui <maoshanghui@flowcapai.com>
2025-11-26 20:12:29 +0800

    dag for notify subscriber

--- dags/rockflow/dags/social.py
+++ dags/rockflow/dags/social.py
@@ -180,3 +180,25 @@ SimpleHttpOperator(
     extra_options={"timeout": 60},
     dag=finalize_copy_trading_plans,
 )
+
+hightlight_subscribers = DAG(
+    "hightlight_subscribers",
+    catchup=False,
+    start_date=pendulum.datetime(2025, 11, 24, tz='America/New_York'),
+    schedule_interval='*/5 * * * *',
+    default_args={
+        "owner": "maomao",
+        "depends_on_past": False,
+        "retries": 0,
+    }
+)
+
+SimpleHttpOperator(
+    task_id='hightlight_subscribers',
+    method='PATCH',
+    http_conn_id='flow-social',
+    endpoint='/social/inner/arena/notify/subscriber?time={{ macros.ds_format(ts, "%Y-%m-%dT%H:%M:%S%z", "%Y-%m-%d %H:%M:%S") }}',
+    response_check=lambda response: response.json()['code'] == 200,
+    extra_options={"timeout": 60},
+    dag=hightlight_subscribers,
+)
