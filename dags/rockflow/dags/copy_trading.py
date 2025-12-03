ac476204bdba435bc91ed16d725b1a1ccd40bd8d
chengwei <2281795275@qq.com>
2025-12-03 17:15:37 +0800

    feat(dags): Split the copy trading DAG tasks for US stocks and Hong Kong stocks
    - 将原有的 copy_trading_copy_position DAG 拆分为美股和港股两个独立任务
    - 新增 copy_trading_copy_position_us 和 copy_trading_copy_position_hk DAG
    - 更新接口端点以支持 market 参数区分市场
    - 将原有的 copy_trading_auto_correction DAG 拆分为美股和港股两个独立任务
    - 新增 copy_trading_auto_correction_us 和 copy_trading_auto_correction_hk DAG
    - 更新自动纠偏接口端点以支持 market 参数区分市场
    - 为所有新任务配置独立的调度时间和时区设置

--- dags/rockflow/dags/copy_trading.py
+++ dags/rockflow/dags/copy_trading.py
@@ -3,8 +3,8 @@ from airflow.models import DAG
 from datetime import datetime, timedelta
 from airflow.providers.http.operators.http import SimpleHttpOperator
 
-copy_trading_copy_position = DAG(
-    "copy_trading_copy_position",
+copy_trading_copy_position_us = DAG(
+    "copy_trading_copy_position_us",
     catchup=False,
     start_date=pendulum.datetime(2024, 10, 14, tz='America/New_York'),
     schedule_interval='35 9 * * *',
@@ -17,17 +17,40 @@ copy_trading_copy_position = DAG(
 )
 
 SimpleHttpOperator(
-    task_id='copy_trading_copy_position',
+    task_id='copy_trading_copy_position_us',
     method='PUT',
     http_conn_id='flow-social',
-    endpoint='/social/inner/copyTrading/position/init',
+    endpoint='/social/inner/copyTrading/position/init?market=US',
     response_check=lambda response: response.json()['code'] == 200,
     extra_options={"timeout": 60},
-    dag=copy_trading_copy_position,
+    dag=copy_trading_copy_position_us,
 )
 
-copy_trading_auto_correction = DAG(
-    "copy_trading_auto_correction",
+copy_trading_copy_position_hk = DAG(
+    "copy_trading_copy_position_hk",
+    catchup=False,
+    start_date=pendulum.datetime(2024, 10, 14, tz='Asia/Shanghai'),
+    schedule_interval='35 9 * * *',
+    default_args={
+        "owner": "chengwei",
+        "depends_on_past": False,
+        "retries": 5,
+        "retry_delay": timedelta(minutes=1),
+    }
+)
+
+SimpleHttpOperator(
+    task_id='copy_trading_copy_position_hk',
+    method='PUT',
+    http_conn_id='flow-social',
+    endpoint='/social/inner/copyTrading/position/init?market=HK',
+    response_check=lambda response: response.json()['code'] == 200,
+    extra_options={"timeout": 60},
+    dag=copy_trading_copy_position_hk,
+)
+
+copy_trading_auto_correction_us = DAG(
+    "copy_trading_auto_correction_us",
     catchup=False,
     start_date=pendulum.datetime(2024, 10, 14, tz='America/New_York'),
     schedule_interval='40 9 * * *',
@@ -40,11 +63,34 @@ copy_trading_auto_correction = DAG(
 )
 
 SimpleHttpOperator(
-    task_id='copy_trading_auto_correction',
+    task_id='copy_trading_auto_correction_us',
     method='PUT',
     http_conn_id='flow-social',
-    endpoint='/social/inner/copyTrading/position/correct?tradeDay={date}'.format(date=datetime.now().strftime("%Y-%m-%d")),
+    endpoint='/social/inner/copyTrading/position/correct?tradeDay={date}&market=US'.format(date=datetime.now().strftime("%Y-%m-%d")),
     response_check=lambda response: response.json()['code'] == 200,
     extra_options={"timeout": 60},
-    dag=copy_trading_auto_correction,
+    dag=copy_trading_auto_correction_us,
+)
+
+copy_trading_auto_correction_hk = DAG(
+    "copy_trading_auto_correction_hk",
+    catchup=False,
+    start_date=pendulum.datetime(2024, 10, 14, tz='Asia/Shanghai'),
+    schedule_interval='40 9 * * *',
+    default_args={
+        "owner": "chengwei",
+        "depends_on_past": False,
+        "retries": 5,
+        "retry_delay": timedelta(minutes=1),
+    }
+)
+
+SimpleHttpOperator(
+    task_id='copy_trading_auto_correction_hk',
+    method='PUT',
+    http_conn_id='flow-social',
+    endpoint='/social/inner/copyTrading/position/correct?tradeDay={date}&market=HK'.format(date=datetime.now().strftime("%Y-%m-%d")),
+    response_check=lambda response: response.json()['code'] == 200,
+    extra_options={"timeout": 60},
+    dag=copy_trading_auto_correction_hk,
 )
