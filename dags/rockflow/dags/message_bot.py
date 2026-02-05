c018a956a3f3710910d9c30f8d3170d8f53577bf
chengwei <2281795275@qq.com>
2026-02-05 18:05:51 +0800

    feat(dags): add face

--- dags/rockflow/dags/message_bot.py
+++ dags/rockflow/dags/message_bot.py
@@ -62,21 +62,12 @@ user_group_send_message_1w = DAG(
     }
 )
 
-SimpleHttpOperator(
-    task_id='user_group_send_message_1w',
-    method='PUT',
-    http_conn_id='flow-admin',
-    endpoint='/admin/inner/user/group/send/1w',
-    response_check=lambda response: response.json()['code'] == 200,
-    extra_options={"timeout": 60},
-    dag=user_group_send_message_1w,
-)
 
-user_group_send_broadcast_1w = DAG(
-    "user_group_send_broadcast_1w",
+face_recognition_send_broadcast = DAG(
+    "face_recognition_send_broadcast",
     catchup=False,
     start_date=pendulum.datetime(2024, 11, 14, tz='Asia/Shanghai'),
-    schedule_interval='0 10 * * 6',
+    schedule_interval='0 10 * * *',
     default_args={
         "owner": "chengwei",
         "depends_on_past": False,
@@ -85,11 +76,57 @@ user_group_send_broadcast_1w = DAG(
 )
 
 SimpleHttpOperator(
-    task_id='user_group_send_broadcast_1w',
+    task_id='face_recognition_send_broadcast',
     method='PUT',
     http_conn_id='flow-admin',
-    endpoint='/admin/inner/user/group/send/broadcast/1w',
+    endpoint='/admin/inner/user/group/faceRecognition/send/BROADCAST',
     response_check=lambda response: response.json()['code'] == 200,
     extra_options={"timeout": 60},
-    dag=user_group_send_broadcast_1w,
+    dag=face_recognition_send_broadcast,
+)
+
+
+face_recognition_send_push = DAG(
+    "face_recognition_send_push",
+    catchup=False,
+    start_date=pendulum.datetime(2024, 11, 14, tz='Asia/Shanghai'),
+    schedule_interval='0 11 * * *',
+    default_args={
+        "owner": "chengwei",
+        "depends_on_past": False,
+        "retries": 0
+    }
+)
+
+SimpleHttpOperator(
+    task_id='face_recognition_send_push',
+    method='PUT',
+    http_conn_id='flow-admin',
+    endpoint='/admin/inner/user/group/faceRecognition/send/PUSH',
+    response_check=lambda response: response.json()['code'] == 200,
+    extra_options={"timeout": 60},
+    dag=face_recognition_send_push,
+)
+
+
+face_recognition_send_email = DAG(
+    "face_recognition_send_email",
+    catchup=False,
+    start_date=pendulum.datetime(2024, 11, 14, tz='Asia/Shanghai'),
+    schedule_interval='0 10 * * 5',
+    default_args={
+        "owner": "chengwei",
+        "depends_on_past": False,
+        "retries": 0
+    }
+)
+
+SimpleHttpOperator(
+    task_id='face_recognition_send_email',
+    method='PUT',
+    http_conn_id='flow-admin',
+    endpoint='/admin/inner/user/group/faceRecognition/send/EMAIL',
+    response_check=lambda response: response.json()['code'] == 200,
+    extra_options={"timeout": 60},
+    dag=face_recognition_send_email,
 )
