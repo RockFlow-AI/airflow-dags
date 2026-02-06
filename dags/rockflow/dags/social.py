b7fe45b224c24102cbf7bd123b83ac3c7be0ef24
maoshanghui <maoshanghui@flowcapai.com>
2026-02-06 11:14:13 +0800

    run sql task

--- dags/rockflow/dags/social.py
+++ dags/rockflow/dags/social.py
@@ -265,3 +265,27 @@ SimpleHttpOperator(
     extra_options={"timeout": 60},
     dag=update_close_copy_plan_order_status,
 )
+
+
+run_sql_stats_task = DAG(
+    "run_sql_stats_task",
+    catchup=False,
+    start_date=pendulum.datetime(2026, 1, 1),
+    schedule_interval=timedelta(minutes=10),
+    default_args={
+        "owner": "momo",
+        "depends_on_past": False,
+        "retries": 0,
+    }
+)
+
+SimpleHttpOperator(
+    task_id='run_sql_stats_task',
+    method='PATCH',
+    http_conn_id='flow-social',
+    endpoint='/social/inner/sqlStats',
+    headers={'accept': '*/*'},
+    response_check=lambda response: response.json()['code'] == 200,
+    extra_options={"timeout": 1000},
+    dag=run_sql_stats_task
+)
