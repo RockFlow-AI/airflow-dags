75bb22fdf511e4c6ea9c07c3a638b523b80d64cf
yuzhiqiang​ <yuzhiqiang@flowcapai.com>
2026-04-03 16:06:41 +0800

    update

--- dags/rockflow/dags/statement_qyzj.py
+++ dags/rockflow/dags/statement_qyzj.py
@@ -3,8 +3,8 @@ from airflow.models import DAG
 from datetime import datetime, timedelta
 from airflow.providers.http.operators.http import SimpleHttpOperator
 
-statement_sync_ftp_file = DAG(
-    "statement_sync_ftp_file",
+qyzj_statement_sync_ftp_file = DAG(
+    "qyzj_statement_sync_ftp_file",
     catchup=False,
     start_date=datetime(2022, 10, 22, 0, 0),
     schedule_interval='50 7,2,1 * * 1-7',
@@ -23,11 +23,11 @@ SimpleHttpOperator(
     endpoint='/inner/statement/ftpFiles/sync?date={date}'.format(date=datetime.now().strftime("%Y%m%d")),
     response_check=lambda response: response.json()['code'] == 200,
     extra_options={"timeout": 600},
-    dag=statement_sync_ftp_file,
+    dag=qyzj_statement_sync_ftp_file,
 )
 
-statement_sync_delay_file = DAG(
-    "statement_sync_delay_file",
+qyzj_statement_sync_delay_file = DAG(
+    "qyzj_statement_sync_delay_file",
     catchup=False,
     start_date=datetime(2022, 10, 22, 0, 0),
     schedule_interval='50 9 * * 1-7',
@@ -46,12 +46,12 @@ SimpleHttpOperator(
     endpoint='/inner/statement/ftpFiles/sync?date={date}'.format(date=datetime.now().strftime("%Y%m%d")),
     response_check=lambda response: response.json()['code'] == 200,
     extra_options={"timeout": 600},
-    dag=statement_sync_delay_file,
+    dag=qyzj_statement_sync_delay_file,
 )
 
 # tradeDetail 兜底
-statement_sync_delay_file_1755 = DAG(
-    "statement_sync_delay_file_1755",
+qyzj_statement_sync_delay_file_1755 = DAG(
+    "qyzj_statement_sync_delay_file_1755",
     catchup=False,
     start_date=datetime(2022, 10, 22, 0, 0),
     schedule_interval='10,30,50 10,11 * * 1-7',
@@ -70,12 +70,12 @@ SimpleHttpOperator(
     endpoint='/inner/statement/ftpFiles/sync?date={date}'.format(date=datetime.now().strftime("%Y%m%d")),
     response_check=lambda response: response.json()['code'] == 200,
     extra_options={"timeout": 600},
-    dag=statement_sync_delay_file_1755,
+    dag=qyzj_statement_sync_delay_file_1755,
 )
 
 # 同步文件20:10兜底
-statement_sync_file_2010 = DAG(
-    "statement_sync_file_2010",
+qyzj_statement_sync_file_2010 = DAG(
+    "qyzj_statement_sync_file_2010",
     catchup=False,
     start_date=datetime(2022, 10, 22, 0, 0),
     schedule_interval='10 12 * * 1-7',
@@ -94,58 +94,12 @@ SimpleHttpOperator(
     endpoint='/inner/statement/ftpFiles/sync?date={date}'.format(date=datetime.now().strftime("%Y%m%d")),
     response_check=lambda response: response.json()['code'] == 200,
     extra_options={"timeout": 600},
-    dag=statement_sync_file_2010,
-)
-
-statement_sync_ftp_file = DAG(
-    "statement_sync_ftp_file",
-    catchup=False,
-    start_date=datetime(2022, 10, 22, 0, 0),
-    schedule_interval='50 7,2,1 * * 1-7',
-    default_args={
-        "owner": "chenborui",
-        "depends_on_past": False,
-        "retries": 3,
-        "retry_delay": timedelta(minutes=30)
-    }
-)
-
-SimpleHttpOperator(
-    task_id='qyzj_statement_sync_ftp_file',
-    method='PATCH',
-    http_conn_id='flow-statement.qyzj',
-    endpoint='/inner/statement/ftpFiles/sync?date={date}'.format(date=datetime.now().strftime("%Y%m%d")),
-    response_check=lambda response: response.json()['code'] == 200,
-    extra_options={"timeout": 600},
-    dag=statement_sync_ftp_file,
-)
-
-statement_sync_delay_file = DAG(
-    "statement_sync_delay_file",
-    catchup=False,
-    start_date=datetime(2022, 10, 22, 0, 0),
-    schedule_interval='50 9 * * 1-7',
-    default_args={
-        "owner": "caoyunfei",
-        "depends_on_past": False,
-        "retries": 3,
-        "retry_delay": timedelta(minutes=30)
-    }
-)
-
-SimpleHttpOperator(
-    task_id='qyzj_statement_sync_delay_file',
-    method='PATCH',
-    http_conn_id='flow-statement.qyzj',
-    endpoint='/inner/statement/ftpFiles/sync?date={date}'.format(date=datetime.now().strftime("%Y%m%d")),
-    response_check=lambda response: response.json()['code'] == 200,
-    extra_options={"timeout": 600},
-    dag=statement_sync_delay_file,
+    dag=qyzj_statement_sync_file_2010,
 )
 
 # 同步文件 T-1
-statement_sync_ftp_file_previous = DAG(
-    "statement_sync_ftp_file_previous",
+qyzj_statement_sync_ftp_file_previous = DAG(
+    "qyzj_statement_sync_ftp_file_previous",
     catchup=False,
     start_date=datetime(2022, 10, 22, 0, 0),
     schedule_interval='50 7 * * 1-7',
@@ -164,12 +118,12 @@ SimpleHttpOperator(
     endpoint='/inner/statement/ftpFiles/sync?date={date}'.format(date=(datetime.now() + timedelta(days=-1)).strftime("%Y%m%d")),
     response_check=lambda response: response.json()['code'] == 200,
     extra_options={"timeout": 600},
-    dag=statement_sync_ftp_file_previous,
+    dag=qyzj_statement_sync_ftp_file_previous,
 )
 
 # 行权11点兜底
-statement_sync_file_23 = DAG(
-    "statement_sync_file_23",
+qyzj_statement_sync_file_23 = DAG(
+    "qyzj_statement_sync_file_23",
     catchup=False,
     start_date=datetime(2022, 10, 22, 0, 0),
     schedule_interval='50 15 * * 1-7',
@@ -188,5 +142,5 @@ SimpleHttpOperator(
     endpoint='/inner/statement/ftpFiles/sync?date={date}'.format(date=datetime.now().strftime("%Y%m%d")),
     response_check=lambda response: response.json()['code'] == 200,
     extra_options={"timeout": 600},
-    dag=statement_sync_file_23,
+    dag=qyzj_statement_sync_file_23,
 )
