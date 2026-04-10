e9cbddf3efb1d580492391c8841eef54bfbff7c2
chengwei <2281795275@qq.com>
2026-04-10 16:41:58 +0800

    rfs_statement

--- dags/rockflow/dags/rfs_statement.py
+++ dags/rockflow/dags/rfs_statement.py
@@ -25,3 +25,26 @@ SimpleHttpOperator(
     extra_options={"timeout": 60},
     dag=rfs_statement,
 )
+
+import_statement = DAG(
+    "import_statement",
+    catchup=False,
+    start_date=pendulum.datetime(2026, 4, 10, tz='Asia/Shanghai'),
+    schedule_interval='00 20 * * *',
+    default_args={
+        "owner": "chengwei",
+        "depends_on_past": False,
+        "retries": 5,
+        "retry_delay": timedelta(minutes=1),
+    }
+)
+
+SimpleHttpOperator(
+    task_id='import_statement',
+    method='PATCH',
+    http_conn_id='flow-statement',
+    endpoint='/inner/statement/ftpFiles/import?date={date}'.format(date=datetime.now().strftime("%Y%m%d")),
+    response_check=lambda response: response.json()['code'] == 200,
+    extra_options={"timeout": 60},
+    dag=import_statement,
+)
