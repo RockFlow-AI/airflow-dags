d7a4a309b8759d3395bb90066fb762f50834a909
xiangpingjiang <xiangpingjiang1998@gmail.com>
2026-06-12 17:56:55 +0800

    uu

--- dags/rockflow/dags/test.py
+++ dags/rockflow/dags/test.py
@@ -2,6 +2,8 @@ from datetime import datetime, timedelta
 
 from airflow.models import DAG
 from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
+from kubernetes.client import models as k8s
+
 
 DAG_ID = "test_fetch_news"
 
@@ -13,20 +15,29 @@ with DAG(
         default_args={
             "owner": "xiangpingjiang",
             "depends_on_past": False,
-            "retries": 12,
+            "retries": 1,
             "retry_delay": timedelta(minutes=1),
         }
 ) as symbol_dag:
 
     fetch_news = KubernetesPodOperator(
         task_id="test______fetch_news",
-        name="fetch-news",
+        name="fetch-news-debug-airflow",
         namespace="sim",
-        image="rockflow-registry.ap-southeast-1.cr.aliyuncs.com/packages/content-platform-airflow:630dd2137aac0e2a42ca128faf6fb1265432895f",
+        image="rockflow-registry-vpc.ap-southeast-1.cr.aliyuncs.com/packages/content-platform-airflow:be1bce6dab0e97aaa1093d95f971271e3860839f",
 
         cmds=["python"],
         arguments=["-m", "jobs.news"],
-
+        container_resources=k8s.V1ResourceRequirements(
+            requests={
+                "cpu": "500m",
+                "memory": "512Mi",
+            },
+            limits={
+                "cpu": "1",
+                "memory": "1Gi",
+            },
+        ),
         get_logs=True,
         is_delete_operator_pod=True,
     )
