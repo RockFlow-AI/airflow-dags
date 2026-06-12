dd9a74d9be4e985e69c4b52c01edd9cee6668590
xiangpingjiang <xiangpingjiang1998@gmail.com>
2026-06-12 15:30:52 +0800

    update

---
+++ dags/rockflow/dags/test.py
@@ -0,0 +1,36 @@
+from datetime import datetime, timedelta
+
+from airflow.models import DAG
+from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
+from kubernetes.client import V1LocalObjectReference
+
+
+DAG_ID = "test_fetch_news"
+
+with DAG(
+        DAG_ID,
+        catchup=False,
+        start_date=datetime(2022, 1, 1),
+        schedule_interval=timedelta(hours=8),
+        default_args={
+            "owner": "xiangpingjiang",
+            "depends_on_past": False,
+            "retries": 12,
+            "retry_delay": timedelta(minutes=1),
+        }
+) as symbol_dag:
+
+    fetch_news = KubernetesPodOperator(
+        task_id="test______fetch_news",
+        name="fetch-news-debug-airflow",
+        namespace="prod",
+        image="rockflow-registry.ap-southeast-1.cr.aliyuncs.com/packages/content-platform-airflow:630dd2137aac0e2a42ca128faf6fb1265432895f",
+
+        cmds=["python"],
+        arguments=["-m", "jobs.news"],
+        image_pull_secrets=[
+            V1LocalObjectReference(name="registry-tmp")
+        ],
+        get_logs=True,
+        is_delete_operator_pod=True,
+    )
