357a66342e58e2b4aa24c0e3554913bb3e9894a4
xiangpingjiang <xiangpingjiang1998@gmail.com>
2026-06-25 01:26:36 +0800

    u

--- dags/rockflow/dags/content-platform/content-platform-dags.py
+++ dags/rockflow/dags/content-platform/content-platform-dags.py
@@ -1,18 +1,29 @@
-from datetime import datetime, timedelta
+from datetime import timedelta
+
 import pendulum
 from airflow.models import DAG
 from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
 from airflow.providers.cncf.kubernetes.secret import Secret
 from kubernetes.client import models as k8s
 
+import os
+
+
+# 依赖数据库连接域名判断环境
+_db_conn = os.environ.get("AIRFLOW_CONN_AIRFLOW_DB", "")
+ENV = "prod" if "rds-prod.rds.rockflow.ai" in _db_conn else "airflow"
+
 secret_file = Secret(
     deploy_type="volume",
     deploy_target="/root/.ssh",
-    secret="prod-ssh-secret",
+    secret="prod-ssh-secret" if ENV == "prod" else "devpod-ssh-secret",
 )
-
-IMAGE = "rockflow-registry-vpc.ap-southeast-1.cr.aliyuncs.com/packages/content-platform-airflow:de95bd0dea94807fb0b4382f26439c201398457d"
-
+image_tag = "de95bd0dea94807fb0b4382f26439c201398457d"
+IMAGES = {
+    "prod": f"rockflow-registry-vpc.ap-southeast-1.cr.aliyuncs.com/packages/content-platform-airflow:{image_tag}",
+    "airflow":  f"rockflow-registry.ap-southeast-1.cr.aliyuncs.com/packages/content-platform-airflow:{image_tag}",
+}
+IMAGE = IMAGES[ENV]
 DEFAULT_ARGS = {
     "owner": "xiangpingjiang",
     "depends_on_past": False,
@@ -24,7 +35,7 @@ DEFAULT_ARGS = {
 def make_dag(dag_id, arguments, params=None, schedule_interval=None, timezone="UTC"):
     with DAG(
         dag_id,
-        tags=["content-platform-airflow-generate"],
+        tags=["content-platform-airflow-data"],
         catchup=False,
         start_date=pendulum.datetime(2022, 1, 1, tz=timezone),
         schedule_interval=schedule_interval,
@@ -34,7 +45,7 @@ def make_dag(dag_id, arguments, params=None, schedule_interval=None, timezone="U
         KubernetesPodOperator(
             task_id=dag_id,
             name=dag_id.replace("_", "-"),
-            namespace="prod",
+            namespace=ENV,
             image=IMAGE,
             cmds=["python"],
             arguments=arguments,
@@ -42,14 +53,63 @@ def make_dag(dag_id, arguments, params=None, schedule_interval=None, timezone="U
                 requests={"cpu": "500m", "memory": "512Mi"},
                 limits={"cpu": "1", "memory": "1Gi"},
             ),
-            env_vars=[
-                k8s.V1EnvVar(name="NAMESPACE", value="prod"),
-            ],
+            env_vars=[k8s.V1EnvVar(name="NAMESPACE", value=ENV)] if ENV == "prod" else [],
             get_logs=True,
             is_delete_operator_pod=True,
             secrets=[secret_file],
         )
 
+##### data
+make_dag(
+    "big_mover_price_us",
+    ["jobs/event_data_source/big_mover_price_us.py"],
+    schedule_interval="30 17 * * *",
+    timezone="America/New_York",
+)
+
+
+make_dag(
+    "big_mover_volume_us",
+    ["jobs/event_data_source/big_mover_volume_us.py"],
+    schedule_interval="30 17 * * *",
+    timezone="America/New_York",
+)
+
+
+make_dag(
+    "dividends_calendar",
+    ["jobs/event_data_source/dividends_calendar.py"],
+    schedule_interval="00 18 * * *",
+    timezone="America/New_York",
+)
+
+
+
+make_dag(
+    "splits_calendar",
+    ["jobs/event_data_source/splits_calendar.py"],
+    schedule_interval="02 18 * * *",
+    timezone="America/New_York",
+)
+
+
+
+make_dag(
+    "earnings_calendar",
+    ["jobs/event_data_source/earnings_calendar.py"],
+    schedule_interval="05 18 * * *",
+    timezone="America/New_York",
+)
+
+
+
+
+
+
+
+
+
+######## generate
 
 make_dag(
     "single_education_card_generate",
